49e6ea4206efb451d2f56a5403e584e44a72af37
Andreas Engberg <adde.engberg@gmail.com>
2024-10-01 16:51:10 +0200

    Update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "rc-dock/dist/rc-dock-dark.css";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <MantineProvider>
+      <div>Hello World</div>
+    </MantineProvider>
+  );
 }
 
 export default App;
