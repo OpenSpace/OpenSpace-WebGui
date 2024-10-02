6f7c05016883f085da82d06d566018ea02881c72
Andreas Engberg <adde.engberg@gmail.com>
2024-10-02 14:28:55 +0200

    add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,63 +1,65 @@
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import tseslint from "typescript-eslint";
-import js from "@eslint/js";
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from 'typescript-eslint';
+import js from '@eslint/js';
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier
+    ],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      "simple-import-sort": simpleImportSort,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort
     },
     linterOptions: {
-      reportUnusedDisableDirectives: "error",
+      reportUnusedDisableDirectives: 'error'
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "@typescript-eslint/no-explicit-any": "error", // Disallow usage of any
-      "no-duplicate-imports": "error", // Imports should be on one line
-      "prefer-destructuring": ["error", { object: true, array: true }],
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-      "simple-import-sort/imports": [
-        "error",
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/no-explicit-any': 'error', // Disallow usage of any
+      'no-duplicate-imports': 'error', // Imports should be on one line
+      'prefer-destructuring': ['error', { object: true, array: true }],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': [
+        'error',
         {
           groups: [
             // Packages `react` related packages come first.
-            ["^react", "^@?\\w"],
+            ['^react', '^@?\\w'],
             // Internal packages.
-            ["^(@|components)(/.*|$)"],
+            ['^(@|components)(/.*|$)'],
             // Side effect imports.
-            ["^\\u0000"],
+            ['^\\u0000'],
             // Parent imports. Put `..` last.
-            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
             // Other relative imports. Put same-folder imports and `.` last.
-            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
             // Style imports.
-            ["^.+\\.?(css)$"],
-          ],
-        },
+            ['^.+\\.?(css)$']
+          ]
+        }
       ],
       // Guards against stupidity
-      "no-self-compare": "error",
-      "no-unreachable-loop": "error",
-      "no-template-curly-in-string": "error", // Catches "${}" template strings
-      "default-case": ["error", { commentPattern: "^skip\\sdefault" }], // require default switch case
-      "default-case-last": "error", // enforce default switch case last
-    },
+      'no-self-compare': 'error',
+      'no-unreachable-loop': 'error',
+      'no-template-curly-in-string': 'error', // Catches "${}" template strings
+      'default-case': ['error', { commentPattern: '^skip\\sdefault' }], // require default switch case
+      'default-case-last': 'error' // enforce default switch case last
+    }
   }
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@/components": "/src/components",
-      "@/public": "/public",
-    },
+      '@/components': '/src/components',
+      '@/public': '/public'
+    }
   },
   server: {
-    port: 4690,
-  },
+    port: 4690
+  }
 });
