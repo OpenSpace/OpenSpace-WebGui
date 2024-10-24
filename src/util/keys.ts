905399c1fd6f467730dda86b28cc56a01cde0a29
Andreas Engberg <adde.engberg@gmail.com>
2024-10-24 09:32:58 +0200

    add util functions

--- src/util/keys.ts
+++ src/util/keys.ts
@@ -1,1 +1,11 @@
+export const NavigationAnchorKey = 'NavigationHandler.OrbitalNavigator.Anchor';
+export const NavigationAimKey = 'NavigationHandler.OrbitalNavigator.Aim';
+export const RetargetAnchorKey = 'NavigationHandler.OrbitalNavigator.RetargetAnchor';
+export const RetargetAimKey = 'NavigationHandler.OrbitalNavigator.RetargetAim';
+
+// To get any scene graph node you need ScenePrefix+NodeIdentifier
+export const ScenePrefixKey = 'Scene.';
+
 export const rootOwnerKey = '__rootOwner';
+
+export const InterestingTagKey = 'GUI.Interesting';
