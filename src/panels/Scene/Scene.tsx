e730fa77ca62f336f5382ff922a00641a4f2f394
Emma Broman <emma.broman@liu.se>
2024-10-25 11:28:48 +0200

    Add some loading blocks to the scene menu

--- src/panels/Scene/Scene.tsx
+++ src/panels/Scene/Scene.tsx
@@ -1,25 +1,45 @@
+import { Skeleton } from '@mantine/core';
+
 import { Property } from '@/components/Property/Property';
+import { useAppSelector } from '@/redux/hooks';
 
 export function Scene() {
+  const propertyOwners = useAppSelector((state) => state.propertyTree.owners.propertyOwners);
+  const hasLoadedScene = Object.keys(propertyOwners).length > 0;
+
+  function loadingBlocks(n: number) {
+    return [...Array(n)].map((_, i) =>
+      <Skeleton key={i} height={8} width={`${Math.random() * 100}%`} radius="xl" />
+    )
+  }
+
   return (
     <div>
-      <p>Scene menu: Testing property types:</p>
-
-      <p>Bool (Earth):</p>
-      <Property uri={'Scene.Earth.Renderable.Enabled'}></Property>
-      <p>String: </p>
-      <Property uri={'Scene.Earth.Renderable.Type'}></Property>
-      <Property uri={'Scene.Earth.Renderable.LightSourceNode'}></Property>
-      <p>Trigger (Earth Blue Marble):</p>
-      <Property
-        uri={'Scene.Earth.Renderable.Layers.ColorLayers.Blue_Marble.Reset'}
-      ></Property>
-      <p>Option (Earth VIIRS):</p>
-      <Property
-        uri={'Scene.Earth.Renderable.Layers.ColorLayers.ESRI_VIIRS_Combo.BlendMode'}
-      ></Property>
-      <p>Selection (Contellation Lines):</p>
-      <Property uri={'Scene.Constellations.Renderable.ConstellationSelection'}></Property>
+      {!hasLoadedScene ?
+        <>
+          <p>Scene menu: Testing property types:</p>
+          {loadingBlocks(4)}
+        </>
+        :
+        <>
+          <p>Scene menu: Testing property types:</p>
+          <p>Bool (Earth):</p>
+          <Property uri={'Scene.Earth.Renderable.Enabled'}></Property>
+          <p>String: </p>
+          <Property uri={'Scene.Earth.Renderable.Type'}></Property>
+          <Property uri={'Scene.Earth.Renderable.LightSourceNode'}></Property>
+          <p>Trigger (Earth Blue Marble):</p>
+          <Property
+            uri={'Scene.Earth.Renderable.Layers.ColorLayers.Blue_Marble.Reset'}
+          ></Property>
+          <p>Option (Earth VIIRS):</p>
+          <Property
+            uri={'Scene.Earth.Renderable.Layers.ColorLayers.ESRI_VIIRS_Combo.BlendMode'}
+          ></Property>
+          <p>Selection (Contellation Lines):</p>
+          <Property uri={'Scene.Constellations.Renderable.ConstellationSelection'}></Property>
+        </>
+      }
     </div>
   );
 }
