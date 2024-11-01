15a9e7c31d922a11f345ffe604734ccae2b1140a
Emma Broman <emma.broman@liu.se>
2024-11-01 11:20:28 +0100

    StringProeprty cleanup

--- src/components/Property/Types/StringProperty.tsx
+++ src/components/Property/Types/StringProperty.tsx
@@ -10,7 +10,6 @@ interface Props {
   value: string;
 }
 
-// TODO: The text edit that sets value on enter should be a more general component
 export function StringProperty({
   name,
   description,
@@ -18,28 +17,6 @@ export function StringProperty({
   setPropertyValue,
   value
 }: Props) {
-  // const [currentValue, setCurrentValue] = useState<string>(value);
-
-  // function onKeyUp(event: React.KeyboardEvent<HTMLInputElement>) {
-  //   if (event.key === 'Enter') {
-  //     setPropertyValue(currentValue);
-  //     event.currentTarget.blur();
-  //   } else if (event.key === 'Escape') {
-  //     setCurrentValue(value);
-  //     event.currentTarget.blur();
-  //   }
-  // }
-
-  // return (
-  //   <TextInput
-  //     value={currentValue}
-  //     onChange={(event) => setCurrentValue(event.currentTarget.value)}
-  //     onKeyUp={(event) => onKeyUp(event)}
-  //     disabled={disabled}
-  //     label={<PropertyLabel label={name} tip={description} />}
-  //   />
-  // );
-
   return <StringInput
     disabled={disabled}
     onEnter={setPropertyValue}
