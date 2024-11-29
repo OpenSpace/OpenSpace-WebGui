ff45da899fd0ffa754bc006b0fb04df7b3adfe2f
Emma Broman <emma.broman@liu.se>
2024-11-29 11:19:36 +0100

    Make renderable panel rocust to missing property owners

--- src/panels/Scene/SceneGraphNode/SceneGraphNode.tsx
+++ src/panels/Scene/SceneGraphNode/SceneGraphNode.tsx
@@ -1,4 +1,4 @@
-import { Tabs } from '@mantine/core';
+import { Tabs, Text } from '@mantine/core';
 
 import { PropertyOwner } from '@/components/PropertyOwner/PropertyOwner';
 import { useAppSelector } from '@/redux/hooks';
@@ -33,36 +33,49 @@ export function SceneGraphNode({ uri }: Props) {
     Other: propertyOwner?.properties
   };
 
+  // Not all transforms are guaranteed to exist
+  tabsData.Transform = tabsData.Transform.filter((transform) => transform !== undefined);
+
   const TabKeys = {
     Renderable: 'Renderable',
     Transform: 'Transform',
     Other: 'Other'
   };
 
+  const hasRenderable = tabsData.Renderable !== undefined;
+  const defaultTab = hasRenderable ? TabKeys.Renderable : TabKeys.Transform;
+
+  // TODO: Include information about the Parentnode under Transform
+  // TODO: Include a tab with Meta information
   return (
     <>
       <SceneGraphNodeHeader uri={uri} />
-      <Tabs variant={'outline'} defaultValue={TabKeys.Renderable}>
+      <Tabs variant={'outline'} defaultValue={defaultTab}>
         <Tabs.List>
-          <Tabs.Tab value={TabKeys.Renderable}>{TabKeys.Renderable}</Tabs.Tab>
+          {hasRenderable && (
+            <Tabs.Tab value={TabKeys.Renderable}>{TabKeys.Renderable}</Tabs.Tab>
+          )}
           <Tabs.Tab value={TabKeys.Transform}>{TabKeys.Transform}</Tabs.Tab>
           <Tabs.Tab value={TabKeys.Other}>{TabKeys.Other}</Tabs.Tab>
         </Tabs.List>
-
-        <Tabs.Panel value={TabKeys.Renderable} p={'sm'}>
-          <PropertyOwner uri={tabsData.Renderable.uri} withHeader={false} />
-        </Tabs.Panel>
-
+        {hasRenderable && (
+          <Tabs.Panel value={TabKeys.Renderable} p={'sm'}>
+            <PropertyOwner uri={tabsData.Renderable!.uri} withHeader={false} />
+          </Tabs.Panel>
+        )}
         <Tabs.Panel value={TabKeys.Transform} p={'sm'}>
-          {tabsData.Transform.map((subowner) => (
-            <PropertyOwner
-              key={subowner.identifier}
-              uri={subowner.uri}
-              expandedOnDefault
-            />
-          ))}
+          {tabsData.Transform.length > 0 ? (
+            tabsData.Transform.map((subowner) => (
+              <PropertyOwner
+                key={subowner.identifier}
+                uri={subowner.uri}
+                expandedOnDefault
+              />
+            ))
+          ) : (
+            <Text>This scene graph node has no transform</Text>
+          )}
         </Tabs.Panel>
-
         <Tabs.Panel value={TabKeys.Other} p={'sm'}>
           <PropertyOwner uri={uri} withHeader={false} hideSubOwners />
         </Tabs.Panel>
