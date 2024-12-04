0b71310aa3eadd577d285a4e520a44b21e59c71b
Ylva Selling <ylva.selling@gmail.com>
2024-12-04 16:04:05 +0100

    Add todo comment for scroll

--- src/panels/UserPanelsPanel/UserPanel.tsx
+++ src/panels/UserPanelsPanel/UserPanel.tsx
@@ -6,6 +6,7 @@ interface Props {
 }
 
 export function UserPage({ src, title }: Props) {
+  // TODO ylvse 2024-12-04: Make this scroll work better
   return (
     <Flex direction={'column'} h={'100vh'}>
       <iframe style={{ flexGrow: 11, border: 0 }} title={title} src={src} />
