e08b39a1090d1768fbc5207692d3e1bfd4da6970
Emma Broman <emma.broman@liu.se>
2024-12-04 14:54:30 +0100

    Use asyncThunk for fetching documentation data

--- src/redux/documentation/documentationMiddleware.ts
+++ src/redux/documentation/documentationMiddleware.ts
@@ -1,3 +1,5 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
 import { api } from '@/api/api';
 import { AppStartListening } from '@/redux/listenerMiddleware';
 
@@ -5,22 +7,22 @@ import { onOpenConnection } from '../connection/connectionSlice';
 
 import { AssetMetaData, initializeDocumentation } from './documentationSlice';
 
-// TODO: Ask Ylva about the async thunk implementation
-const getDocumentation = async (callback: (data: AssetMetaData[]) => void) => {
-  // @TODO (emmbr, 2024-12-04): We could also get the rest of the documentation data here.
-  // Like keybindings, lua function descriptions, etc., but for now we only get the meta
-  // data
-  const assetsMetaData = await api.getDocumentation('meta');
-  callback(assetsMetaData as AssetMetaData[]);
-};
+export const fetchDocumentation = createAsyncThunk(
+  'documentation/fetchDocumentation',
+  async (_, thunkAPI) => {
+    // @TODO (emmbr, 2024-12-04): We could also get the rest of the documentation data
+    // here. Like keybindings, lua function descriptions, etc., but for now we only get
+    // the meta data
+    const assetsMetaData = (await api.getDocumentation('meta')) as AssetMetaData[];
+    thunkAPI.dispatch(initializeDocumentation(assetsMetaData));
+  }
+);
 
 export const addDocumentationListener = (startListening: AppStartListening) => {
   startListening({
     actionCreator: onOpenConnection,
     effect: async (_, listenerApi) => {
-      getDocumentation((data) => {
-        listenerApi.dispatch(initializeDocumentation(data));
-      });
+      listenerApi.dispatch(fetchDocumentation());
     }
   });
 };
