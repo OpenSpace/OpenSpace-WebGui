109dfaf23137331176b0fda63a8741351ff0ce53
Emma Broman <emma.broman@liu.se>
2024-12-10 11:05:09 +0100

    Add a button for resetting Scene menu filters

--- src/panels/Scene/Scene.tsx
+++ src/panels/Scene/Scene.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import {
   ActionIcon,
+  Button,
   Checkbox,
   Container,
   Group,
@@ -30,6 +31,13 @@ export function Scene() {
 
   const [showOnlyVisible, setshowOnlyVisible] = useState(false);
   const [showHiddenNodes, setShowHiddenNodes] = useState(false);
+
+  const hasFilters = showOnlyVisible || showHiddenNodes || selectedTags.length > 0;
+  function clearFilters() {
+    setshowOnlyVisible(false);
+    setShowHiddenNodes(false);
+    setSelectedTags([]);
+  }
 
   function loadingBlocks(n: number, minWidthPercentage: number = 0) {
     const min = minWidthPercentage;
@@ -65,45 +73,56 @@ export function Scene() {
         <Container>
           <Group justify={'space-between'}>
             <Title order={2}>Scene</Title>
-            {/* TODO: Move this settings menu to a separate component */}
-            <Menu position={'right-start'} closeOnItemClick={false}>
-              <Menu.Target>
-                <ActionIcon>
-                  <FilterIcon />
-                </ActionIcon>
-              </Menu.Target>
-              <Menu.Dropdown maw={'300px'}>
-                <Group>
-                  <Checkbox
-                    label={'Show only visible'}
-                    checked={showOnlyVisible}
-                    onChange={(event) => setshowOnlyVisible(event.currentTarget.checked)}
+            <Group>
+              {hasFilters && (
+                <Button size={'compact-sm'} onClick={clearFilters}>
+                  Reset filters
+                </Button>
+              )}
+              {/* TODO: Move this settings menu to a separate component */}
+              <Menu position={'right-start'} closeOnItemClick={false}>
+                <Menu.Target>
+                  <ActionIcon>
+                    <FilterIcon />
+                  </ActionIcon>
+                </Menu.Target>
+                <Menu.Dropdown maw={'300px'}>
+                  <Group>
+                    <Checkbox
+                      label={'Show only visible'}
+                      checked={showOnlyVisible}
+                      onChange={(event) =>
+                        setshowOnlyVisible(event.currentTarget.checked)
+                      }
+                    />
+                    <Tooltip text={'Visible = Enabled and not faded out'} />
+                  </Group>
+                  <Group>
+                    <Checkbox
+                      label={'Show objects with GUI hidden flag'}
+                      checked={showHiddenNodes}
+                      onChange={(event) =>
+                        setShowHiddenNodes(event.currentTarget.checked)
+                      }
+                    />
+                    <Tooltip
+                      text={
+                        'Show scene graph nodes that are marked as hidden in the GUI ' +
+                        'part of the asset. These are otherwise hidden in the interface'
+                      }
+                    />
+                  </Group>
+                  <Title order={3}>Tags</Title>
+                  <MultiSelect
+                    data={sortedTags}
+                    value={selectedTags}
+                    onChange={setSelectedTags}
+                    clearable
+                    searchable
                   />
-                  <Tooltip text={'Visible = Enabled and not faded out'} />
-                </Group>
-                <Group>
-                  <Checkbox
-                    label={'Show objects with GUI hidden flag'}
-                    checked={showHiddenNodes}
-                    onChange={(event) => setShowHiddenNodes(event.currentTarget.checked)}
-                  />
-                  <Tooltip
-                    text={
-                      'Show scene graph nodes that are marked as hidden in the GUI ' +
-                      'part of the asset. These are otherwise hidden in the interface'
-                    }
-                  />
-                </Group>
-                <Title order={3}>Tags</Title>
-                <MultiSelect
-                  data={sortedTags}
-                  value={selectedTags}
-                  onChange={setSelectedTags}
-                  clearable
-                  searchable
-                />
-              </Menu.Dropdown>
-            </Menu>
+                </Menu.Dropdown>
+              </Menu>
+            </Group>
           </Group>
           <SceneTree
             filter={{
