c30439937060ebacf1da13f677ddac6103477c5d
Andreas Engberg <adde.engberg@gmail.com>
2024-12-17 16:10:36 +0100

    populate help menu

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -8,6 +8,7 @@ import { propertiesReducer } from './propertytree/properties/propertiesSlice';
 import { propertyOwnersReducer } from './propertytree/propertyowner/propertyOwnerSlice';
 import { sessionRecordingReducer } from './sessionrecording/sessionRecordingSlice';
 import { timeReducer } from './time/timeSlice';
+import { versionReducer } from './version/versionSlice';
 import { listenerMiddleware } from './listenerMiddleware';
 
 export const store = configureStore({
@@ -19,7 +20,8 @@ export const store = configureStore({
     properties: propertiesReducer,
     propertyOwners: propertyOwnersReducer,
     sessionRecording: sessionRecordingReducer,
-    time: timeReducer
+    time: timeReducer,
+    version: versionReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([listenerMiddleware.middleware]),
---
+++ src/redux/version/versionMiddleware.ts
@@ -0,0 +1,23 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import { api } from '@/api/api';
+import { onOpenConnection } from '@/redux/connection/connectionSlice';
+import { AppStartListening } from '@/redux/listenerMiddleware';
+
+import { OpenSpaceVersion } from './versionSlice';
+
+export const getVersion = createAsyncThunk('version/getVersion', async () => {
+  const topic = api.startTopic('version', {});
+  const { value } = await topic.iterator().next();
+  topic.cancel();
+  return value as OpenSpaceVersion;
+});
+
+export const addVersionListener = (startListening: AppStartListening) => {
+  startListening({
+    actionCreator: onOpenConnection,
+    effect: async (_, listenerApi) => {
+      listenerApi.dispatch(getVersion());
+    }
+  });
+};
