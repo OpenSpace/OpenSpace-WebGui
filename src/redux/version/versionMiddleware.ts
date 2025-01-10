28cc329903c3ce5ff675020761f4d29cd25552c8
Ylva Selling <ylva.selling@gmail.com>
2025-01-10 11:31:42 +0100

    Lint fix

--- src/redux/version/versionMiddleware.ts
+++ src/redux/version/versionMiddleware.ts
@@ -3,6 +3,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { api } from '@/api/api';
 import { onOpenConnection } from '@/redux/connection/connectionSlice';
 import { AppStartListening } from '@/redux/listenerMiddleware';
+
 import { OpenSpaceVersionInfoState } from './versionSlice';
 
 export const getVersion = createAsyncThunk('version/getVersion', async () => {
