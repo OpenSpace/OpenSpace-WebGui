3ee5eef82cc1e2c5bd3d8647b099fdd2dc440f2d
Ylva Selling <ylva.selling@gmail.com>
2025-01-17 11:59:05 +0100

    Add error boundary to window component

--- src/windowmanagement/Window/Window.tsx
+++ src/windowmanagement/Window/Window.tsx
@@ -1,10 +1,17 @@
 import { PropsWithChildren } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { useDispatch } from 'react-redux';
 import { ScrollArea } from '@mantine/core';
 import { useElementSize } from '@mantine/hooks';
+
+import { reloadPropertyTree } from '@/redux/propertytree/propertyTreeMiddleware';
+
+import { fallbackRender } from './fallbackRenderer';
 import { WindowSizeContext } from './WindowSizeContext';
 
 export function Window({ children }: PropsWithChildren) {
   const { ref, width, height } = useElementSize();
+  const dispatch = useDispatch();
   return (
     <WindowSizeContext.Provider
       value={{
@@ -13,7 +20,12 @@ export function Window({ children }: PropsWithChildren) {
       }}
     >
       <ScrollArea h={'100%'} ref={ref}>
-        {children}
+        <ErrorBoundary
+          fallbackRender={fallbackRender}
+          onReset={() => dispatch(reloadPropertyTree())}
+        >
+          {children}
+        </ErrorBoundary>
       </ScrollArea>
     </WindowSizeContext.Provider>
   );
