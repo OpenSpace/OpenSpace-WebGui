0ae9705d9a21314002610ba27e1671388b7c8824
Ylva Selling <ylva.selling@gmail.com>
2025-01-27 14:03:11 +0100

    Refactor the wwt hooks

--- src/panels/SkyBrowserPanel/WorldWideTelescope/hooks.ts
+++ src/panels/SkyBrowserPanel/WorldWideTelescope/hooks.ts
@@ -4,12 +4,9 @@ import { useGetStringPropertyValue } from '@/api/hooks';
 
 import { Messages } from './types';
 
-export function useSendMessageToWwt() {
-  const [wwtHasLoaded, setWwtHasLoaded] = useState(false);
-  const [url] = useGetStringPropertyValue('Modules.SkyBrowser.WwtImageCollectionUrl');
-
-  const [imageCollectionLoaded, setImageCollectionLoaded] = useState(false);
-  const setSetupWwtFunc = useRef<ReturnType<typeof setInterval> | null>(null);
+// This hook defines the messages WWT can receive and provides the ref
+// that should be attached to the iframe of WWT
+function useMessages() {
   const ref = useRef<HTMLIFrameElement>(null);
 
   const sendMessageToWwt = useCallback((message: Messages) => {
@@ -23,78 +20,6 @@ export function useSendMessageToWwt() {
       console.error(e);
     }
   }, []);
-
-  const hideChrome = useCallback(() => {
-    sendMessageToWwt({
-      event: 'modify_settings',
-      settings: [['hideAllChrome', true]],
-      target: 'app'
-    });
-  }, [sendMessageToWwt]);
-
-  const loadImageCollection = useCallback(
-    (url: string) => {
-      sendMessageToWwt({
-        event: 'load_image_collection',
-        url: url,
-        loadChildFolders: true
-      });
-    },
-    [sendMessageToWwt]
-  );
-
-  // Add event listeners for World Wide Telescope responses
-  useEffect(() => {
-    function handleCallbackMessage(event) {
-      // We got the first response from wwt - now load the image collection
-      // and hide the settings icons on the side
-      if (event.data === 'wwt_has_loaded') {
-        setWwtHasLoaded(true);
-        hideChrome();
-        if (url) {
-          loadImageCollection(url);
-        }
-      }
-      // Image collection finished loading
-      if (event.data === 'load_image_collection_completed') {
-        setImageCollectionLoaded(true);
-        console.log('Image collecvtion is loaded');
-      }
-    }
-
-    window.addEventListener('message', handleCallbackMessage);
-    return () => window.removeEventListener('message', handleCallbackMessage);
-  }, [hideChrome, loadImageCollection, url]);
-
-  const setAim = useCallback(
-    (ra: number, dec: number, fov: number, roll: number) => {
-      sendMessageToWwt({
-        event: 'center_on_coordinates',
-        ra,
-        dec,
-        fov,
-        roll,
-        instant: true
-      });
-    },
-    [sendMessageToWwt]
-  );
-
-  // Start send messages to World Wide Telescope to trigger a response
-  useEffect(() => {
-    if (!wwtHasLoaded && url !== undefined) {
-      // Send aim messages to WorldWide Telescope to prompt it to reply with a message
-      setSetupWwtFunc.current = setInterval(setAim, 250);
-    }
-    // When WorldWide Telescope has replied with a message, stop sending it unnecessary messages
-    return () => {
-      if (!setSetupWwtFunc.current) {
-        return;
-      }
-      clearInterval(setSetupWwtFunc.current);
-      setSetupWwtFunc.current = null;
-    };
-  }, [url, wwtHasLoaded, setAim]);
 
   const setBorderRadius = useCallback(
     (radius: number) => {
@@ -115,6 +40,129 @@ export function useSendMessageToWwt() {
     },
     [sendMessageToWwt]
   );
+
+  const hideChrome = useCallback(() => {
+    sendMessageToWwt({
+      event: 'modify_settings',
+      settings: [['hideAllChrome', true]],
+      target: 'app'
+    });
+  }, [sendMessageToWwt]);
+
+  const loadImageCollection = useCallback(
+    (url: string) => {
+      sendMessageToWwt({
+        event: 'load_image_collection',
+        url: url,
+        loadChildFolders: true
+      });
+    },
+    [sendMessageToWwt]
+  );
+
+  const setAim = useCallback(
+    (ra: number, dec: number, fov: number, roll: number) => {
+      sendMessageToWwt({
+        event: 'center_on_coordinates',
+        ra,
+        dec,
+        fov,
+        roll,
+        instant: true
+      });
+    },
+    [sendMessageToWwt]
+  );
+
+  return {
+    ref,
+    setBorderColor,
+    setBorderRadius,
+    hideChrome,
+    loadImageCollection,
+    setAim
+  };
+}
+
+// This hook will set up message listeners so we can collect the messages wwt sends
+// Once wwt has loaded, we load the image collection
+function useWwtEventListener() {
+  const [wwtHasLoaded, setWwtHasLoaded] = useState(false);
+  const [imageCollectionLoaded, setImageCollectionLoaded] = useState(false);
+
+  // Add event listeners for World Wide Telescope responses
+  useEffect(() => {
+    function handleCallbackMessage(event: MessageEvent) {
+      // We got the first response from wwt - now load the image collection
+      // and hide the settings icons on the side
+      if (event.data === 'wwt_has_loaded') {
+        setWwtHasLoaded(true);
+      }
+      // Image collection finished loading
+      if (event.data === 'load_image_collection_completed') {
+        setImageCollectionLoaded(true);
+      }
+    }
+
+    window.addEventListener('message', handleCallbackMessage);
+    return () => window.removeEventListener('message', handleCallbackMessage);
+  }, []);
+
+  return { wwtHasLoaded, imageCollectionLoaded };
+}
+
+// This hook will start sending messages to WorldWideTelescope to trigger a response
+// Once a response has been given we cancel the pinging
+function useStartConnection(
+  connect: boolean,
+  setAim: (ra: number, dec: number, fov: number, roll: number) => void
+) {
+  const setSetupWwtFunc = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  // Start send messages to World Wide Telescope to trigger a response
+  useEffect(() => {
+    if (connect) {
+      // Send aim messages to WorldWide Telescope to prompt it to reply with a message
+      setSetupWwtFunc.current = setInterval(setAim, 250);
+    }
+    // When WorldWide Telescope has replied with a message, stop sending it unnecessary messages
+    return () => {
+      if (!setSetupWwtFunc.current) {
+        return;
+      }
+      clearInterval(setSetupWwtFunc.current);
+      setSetupWwtFunc.current = null;
+    };
+  }, [connect, setAim]);
+}
+
+export function useSendMessageToWwt() {
+  const {
+    ref,
+    setAim,
+    setBorderColor,
+    setBorderRadius,
+    hideChrome,
+    loadImageCollection
+  } = useMessages();
+  // Set up the message listener which will tell us once wwt has been loaded
+  // and when the image collection has been loaded
+  const { imageCollectionLoaded, wwtHasLoaded } = useWwtEventListener();
+
+  // Start pinging WWT with the set aim function
+  // We want to make sure we have the image collection url so wait for that
+  const [url] = useGetStringPropertyValue('Modules.SkyBrowser.WwtImageCollectionUrl');
+  const connect = !wwtHasLoaded && url !== undefined;
+  useStartConnection(connect, setAim);
+
+  // Once wwt has been loaded, we pass messages to hide chrome and load the image
+  // collection
+  useEffect(() => {
+    if (wwtHasLoaded && url !== undefined) {
+      hideChrome();
+      loadImageCollection(url);
+    }
+  }, [wwtHasLoaded]);
 
   return {
     ref,
