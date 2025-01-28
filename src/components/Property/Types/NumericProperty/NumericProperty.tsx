acc4e683e01c99ff98cb434bd3b47167d52839ca
Emma Broman <emma.broman@liu.se>
2025-01-28 16:47:05 +0100

    Start working on new numeric sliders

--- src/components/Property/Types/NumericProperty/NumericProperty.tsx
+++ src/components/Property/Types/NumericProperty/NumericProperty.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
-import { MdUnfoldMore } from 'react-icons/md';
-import { ActionIcon, Box, Group } from '@mantine/core';
+import { useState } from 'react';
+import { Group, NumberFormatter, Slider } from '@mantine/core';
+import { scalePow } from 'd3';
 
 import { NumericInput } from '@/components/Input/NumericInput';
 import { PropertyLabel } from '@/components/Property/PropertyLabel';
 
-import { NumericPropertySlider } from './Views/Slider';
+import classes from './NumericProperty.module.css';
 
 interface Props {
   name: string;
@@ -31,44 +31,92 @@ export function NumericProperty({
   additionalData
 }: Props) {
   const [currentValue, setCurrentValue] = useState<number>(value);
-  const [currentViewIndex, setCurrentViewIndex] = useState<number>(0);
 
-  useEffect(() => {
-    setCurrentValue(value);
-  }, [value]);
+  // TODO: Figure out a nice way to handle this, i.e. update the current value when the
+  // value property changes
+  // useEffect(() => {
+  //   setCurrentValue(value);
+  // }, [value]);
 
   const min = additionalData.MinimumValue;
   const max = additionalData.MaximumValue;
   const step = additionalData.SteppingValue;
   const exponent = additionalData.Exponent;
 
-  const views = [
-    <NumericPropertySlider
-      disabled={disabled}
-      min={min}
-      max={max}
-      step={step}
-      exponent={exponent}
-    />,
-    <NumericInput defaultValue={currentValue} disabled={disabled} />
+  const scale = scalePow().exponent(exponent).domain([min, max]).range([min, max]);
+
+  const getDecimalPlaces = (num: number) => {
+    const match = ('' + num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+    if (!match) {
+      return 0;
+    }
+    return Math.max(
+      0,
+      // Number of digits right of decimal point.
+      (match[1] ? match[1].length : 0) -
+        // Adjust for scientific notation.
+        (match[2] ? +match[2] : 0)
+    );
+  };
+
+  const decimalPlaces = getDecimalPlaces(step);
+
+  // TODO: Figure out how to ha
+
+  function markLabel(value: number): JSX.Element | string {
+    return value < 100000 ? (
+      <NumberFormatter value={value} decimalScale={decimalPlaces} />
+    ) : (
+      value.toPrecision(1)
+    );
+  }
+
+  // TODO: Only create marks for valid steps
+  const marksStep = (max - min) / 4;
+  const marks = [
+    { value: min, label: markLabel(min) },
+    { value: min + 1 * marksStep },
+    { value: min + 2 * marksStep, label: markLabel(min + 2 * marksStep) },
+    { value: min + 3 * marksStep },
+    { value: max, label: markLabel(max) }
   ];
+
+  const scaledMarks = marks.map((mark) => ({
+    value: scale.invert(mark.value),
+    label: mark.label
+  }));
 
   return (
     <>
       <PropertyLabel label={name} tip={description} />
-      <Group>
-        <Box flex={1}>{views[currentViewIndex]}</Box>
-        <ActionIcon
-          flex={0}
-          variant={'default'}
-          onClick={() =>
-            setCurrentViewIndex(
-              currentViewIndex < views.length - 1 ? currentViewIndex + 1 : 0
-            )
-          }
-        >
-          <MdUnfoldMore />
-        </ActionIcon>
+      <Group align={'bottom'}>
+        <Slider
+          flex={1}
+          disabled={disabled}
+          label={(value) => (
+            <NumberFormatter value={value} decimalScale={decimalPlaces} />
+          )}
+          value={currentValue}
+          min={min}
+          max={max}
+          step={step}
+          marks={scaledMarks}
+          scale={(value) => scale(value)}
+          onChange={(value) => {
+            setCurrentValue(value);
+            setPropertyValue(value);
+          }}
+          classNames={classes}
+        />
+        <Group w={'100px'}>
+          <NumericInput
+            defaultValue={currentValue}
+            disabled={disabled}
+            min={min}
+            max={max}
+            step={step}
+          />
+        </Group>
       </Group>
     </>
   );
