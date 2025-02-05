020bf451d8fc2ada4ab627275b73c09801768bb2
Emma Broman <emma.broman@liu.se>
2025-02-05 16:39:44 +0100

    Prevent vector properties from shrinking too much

--- src/components/Property/Types/VectorProperty/ViewOptions/DefaultValueList.tsx
+++ src/components/Property/Types/VectorProperty/ViewOptions/DefaultValueList.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@mantine/core';
+import { Box, Flex } from '@mantine/core';
 
 import { NumericInput } from '@/components/Input/NumericInput';
 import { VectorPropertyProps } from '@/components/Property/Types/VectorProperty/VectorProperty';
@@ -22,15 +22,16 @@ export function ValueList({
   return (
     <Flex gap={'xs'}>
       {value.map((v, i) => (
-        <NumericInput
-          key={i}
-          defaultValue={v}
-          disabled={disabled}
-          min={min[i]}
-          max={max[i]}
-          step={step[i]}
-          onEnter={(newValue) => setValue(i, newValue)}
-        />
+        <Box miw={70} key={i}>
+          <NumericInput
+            defaultValue={v}
+            disabled={disabled}
+            min={min[i]}
+            max={max[i]}
+            step={step[i]}
+            onEnter={(newValue) => setValue(i, newValue)}
+          />
+        </Box>
       ))}
     </Flex>
   );
