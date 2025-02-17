bf9258e0f57228fa3a2d5d68c27e4427133a5c58
Emma Broman <emma.broman@liu.se>
2025-02-17 15:56:44 +0100

    Clean up code for default value list vector input

--- src/components/Property/Types/VectorProperty/ViewOptions/DefaultValueList.tsx
+++ src/components/Property/Types/VectorProperty/ViewOptions/DefaultValueList.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from '@mantine/core';
+import { Flex } from '@mantine/core';
 
 import { NumericInput } from '@/components/Input/NumericInput/NumericInput';
 import { VectorPropertyProps } from '@/components/Property/Types/VectorProperty/VectorProperty';
@@ -22,17 +22,16 @@ export function ValueList({
   return (
     <Flex gap={'xs'}>
       {value.map((v, i) => (
-        <Box miw={70} key={i}>
-          <NumericInput
-            key={i}
-            value={v}
-            disabled={disabled}
-            min={min[i]}
-            max={max[i]}
-            step={step[i]}
-            onEnter={(newValue) => setValue(i, newValue)}
-          />
-        </Box>
+        <NumericInput
+          miw={40}
+          key={i}
+          value={v}
+          disabled={disabled}
+          min={min[i]}
+          max={max[i]}
+          step={step[i]}
+          onEnter={(newValue) => setValue(i, newValue)}
+        />
       ))}
     </Flex>
   );
