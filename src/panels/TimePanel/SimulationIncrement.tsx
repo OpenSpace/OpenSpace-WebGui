544bab5f2893a5a86d4c88629a4cd9762242993e
Emma Broman <emma.broman@liu.se>
2025-02-17 11:24:34 +0100

    Move QuickAdjust to separate component and fix some issues

--- src/panels/TimePanel/SimulationIncrement.tsx
+++ src/panels/TimePanel/SimulationIncrement.tsx
@@ -1,19 +1,18 @@
 import { useRef, useState } from 'react';
-import { Group, Select, Slider, Text } from '@mantine/core';
+import { Group, Select } from '@mantine/core';
 import { useThrottledCallback } from '@mantine/hooks';
 
 import { useOpenSpaceApi } from '@/api/hooks';
 import { useAppSelector } from '@/redux/hooks';
 
 import { DeltaTimeStepsControl } from './DeltaTimeStepControl';
+import { QuickAdjustSlider } from './QuickAdjustSlider';
 import { TimeIncrementInput } from './TimeIncrementInput';
 import { Decimals, StepSizes, TimePart } from './types';
 
 export function SimulationIncrement() {
   const updateDeltaTime = useThrottledCallback(updateDeltaTimeNow, 50);
   const [stepSize, setStepSize] = useState<TimePart>(TimePart.Seconds);
-  const [beforeQuickAdjust, setBeforeQuickAdjust] = useState<number>(0);
-  const [sliderValue, setSliderValue] = useState(0);
 
   const luaApi = useOpenSpaceApi();
   const targetDeltaTime = useAppSelector((state) => state.time.targetDeltaTime) ?? 1;
@@ -38,24 +37,14 @@ export function SimulationIncrement() {
     updateDeltaTime(newDeltaTime);
   }
 
-  function setQuickAdjust(value: number) {
-    // Store the current deltaTime, because of capture this value will be the same as long
-    // as the first time user pressed the slider
-    setBeforeQuickAdjust(targetDeltaTime);
-
+  function onQuickAdjust(value: number) {
     const quickAdjust = StepSizes[stepSize] * value ** 5;
-    updateDeltaTime(quickAdjust);
-
-    setSliderValue(value);
+    updateDeltaTime(targetDeltaTime + quickAdjust);
   }
-
-  const sliderMax = 10;
-  const percentagePerStep = 100 / (sliderMax * 2);
-  const sliderWidth = `${Math.abs(sliderValue * percentagePerStep)}%`;
 
   return (
     <>
-      <Group grow>
+      <Group grow mb={'xs'}>
         <Select
           label={'Display Unit'}
           value={stepSize}
@@ -72,39 +61,9 @@ export function SimulationIncrement() {
           wrapStepControlButtons={false}
         />
       </Group>
-      <Group mt={'xs'} justify={'space-between'}>
-        <Text size={'md'} c={'dimmed'}>
-          -
-        </Text>
-        <Text size={'md'} c={'dimmed'}>
-          Quick Adjust
-        </Text>
-        <Text size={'md'} c={'dimmed'}>
-          +
-        </Text>
-      </Group>
-      <Slider
-        mb={'xl'}
-        min={-sliderMax}
-        max={sliderMax}
-        onChange={setQuickAdjust}
-        onChangeEnd={() => {
-          // Reset the slider back to middle and set the deltaTime to whatever it was
-          // before we started adjusting
-          setSliderValue(0);
-          updateDeltaTime(beforeQuickAdjust);
-        }}
-        label={null}
-        step={0.05}
-        value={sliderValue}
-        marks={[{ value: 0 }]}
-        // Make the bar start from the center
-        styles={{
-          bar: {
-            left: sliderValue > 0 ? '50%' : `calc(50% - ${sliderWidth})`,
-            width: `${sliderWidth}`
-          }
-        }}
+      <QuickAdjustSlider
+        onChange={onQuickAdjust}
+        onEnd={() => updateDeltaTime(targetDeltaTime)}
       />
       <DeltaTimeStepsControl stepSize={stepSize} />
     </>
