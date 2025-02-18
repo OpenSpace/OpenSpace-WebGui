2c6f6564d2991b3fe2421e02b2d3612fdddae7c6
Emma Broman <emma.broman@liu.se>
2025-02-18 09:34:06 +0100

    Undo decimal scale setting

--- src/components/Property/Types/NumericProperty/NumericProperty.tsx
+++ src/components/Property/Types/NumericProperty/NumericProperty.tsx
@@ -71,7 +71,6 @@ export function NumericProperty({
           <NumericInput
             value={currentValue}
             disabled={disabled}
-            decimalScale={5}
             min={min}
             max={max}
             step={step}
