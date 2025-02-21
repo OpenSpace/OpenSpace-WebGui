7c420821f46e50fb3970665d56caefbae52e168a
Emma Broman <emma.broman@liu.se>
2025-02-21 17:09:31 +0100

    Lint format time.ts (too many things to comment on)

--- src/util/time.ts
+++ src/util/time.ts
@@ -4,30 +4,29 @@ export function dateToOpenSpaceTimeString(date: Date): string {
   // is given.
 
   const year = date.getFullYear();
-  
-  // Spice assumes that years between 0-68 are abbreviation of 2000-2068 and 69-99 are 
+
+  // Spice assumes that years between 0-68 are abbreviation of 2000-2068 and 69-99 are
   // abbreviation of 1969-1999, we assume people mean the actual year so we send the date
   // with an epoch as well. see: https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/str2et_c.html
   // for further details
-  if(year >= 0 && year < 100) {
-    const [utcYear, month, rest] = date.toJSON().replace('Z','').split('-')
+  if (year >= 0 && year < 100) {
+    const [utcYear, month, rest] = date.toJSON().replace('Z', '').split('-');
     // With the format YYYY A.D. MM DD HH:MM:SS we can't have the 'T' token
     const [day, time] = rest.split('T');
-    return `${utcYear} A.D. ${month} ${day} ${time}`
+    return `${utcYear} A.D. ${month} ${day} ${time}`;
   }
 
   // For negative years we have to parse the string in B.C. epoch format for spice
   // see: https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/str2et_c.html
-  if(year < 0) {
+  if (year < 0) {
     // Convert date to UTC time and replace Z as always, we also remove the preceding '-'
     // since we set the date using B.C epoch
-    const utcDate = date.toJSON().replace('Z','').substring(1)
-    const [utcYear, month, rest] = utcDate.split('-')
+    const utcDate = date.toJSON().replace('Z', '').substring(1);
+    const [utcYear, month, rest] = utcDate.split('-');
     // With the format YYYY B.C. MM DD HH:MM:SS we can't have the 'T' token
     const [day, time] = rest.split('T');
 
-    return `${utcYear} B.C. ${month} ${day} ${time}`
-
+    return `${utcYear} B.C. ${month} ${day} ${time}`;
   }
 
   // For years > 10 000 the JSON string includes a '+' which mess up the OpenSpace
