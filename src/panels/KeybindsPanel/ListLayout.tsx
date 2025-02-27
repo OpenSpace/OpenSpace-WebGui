f4ca2546bb343e9936c0f4f561dd89802fd4a77d
Andreas Engberg <adde.engberg@gmail.com>
2025-02-27 13:58:14 +0100

    Add modifiers filter using chip

--- src/panels/KeybindsPanel/ListLayout.tsx
+++ src/panels/KeybindsPanel/ListLayout.tsx
@@ -1,21 +1,26 @@
 import { useState } from 'react';
-import { Button, Text } from '@mantine/core';
+import { Button, Chip, Group, Text } from '@mantine/core';
 
 import { FilterList } from '@/components/FilterList/FilterList';
 import { generateMatcherFunctionByKeys } from '@/components/FilterList/util';
 import { Layout } from '@/components/Layout/Layout';
 import { useAppSelector } from '@/redux/hooks';
-import { Action } from '@/types/types';
+import { Action, KeybindModifiers } from '@/types/types';
 
 import { KeybindButtons } from './KeybindButtons';
 import { KeybindInfo } from './KeybindInfo';
 
 export function ListLayout() {
   const [selectedAction, setSelectedAction] = useState<Action | null>(null);
+  const [modifiersFilter, setModifiersFilter] = useState<KeybindModifiers>([]);
   const keybinds = useAppSelector((state) => state.actions.keybinds);
   const actions = useAppSelector((state) => state.actions.actions);
 
   const keybindInfo = keybinds
+    .filter((keybind) => {
+      // Filter all keybinds based on the modifiers selected
+      return modifiersFilter.every((modifier) => keybind.modifiers.includes(modifier));
+    })
     .map((keybind) => {
       const action = actions.find((action) => action.identifier === keybind.action);
 
@@ -33,20 +38,39 @@ export function ListLayout() {
       return a.key > b.key ? 1 : -1;
     });
 
+  function onClick(action: Action): void {
+    if (action.identifier === selectedAction?.identifier) {
+      setSelectedAction(null);
+    } else {
+      setSelectedAction(action);
+    }
+  }
+
   return (
     <Layout>
       <Layout.GrowingSection pb={'md'}>
         <FilterList>
-          <FilterList.InputField placeHolderSearchText={'Search for a keybind'} />
+          <Group gap={'xs'}>
+            <FilterList.InputField
+              placeHolderSearchText={'Search for a keybind'}
+              flex={1}
+              miw={150}
+            />
+            <Chip.Group
+              multiple
+              onChange={(value) => setModifiersFilter(value as KeybindModifiers)}
+            >
+              <Chip value={'shift'}>Shift</Chip>
+              <Chip value={'control'}>Ctrl</Chip>
+              <Chip value={'alt'}>Alt</Chip>
+              <Chip value={'super'}>Super</Chip>
+            </Chip.Group>
+          </Group>
           <FilterList.SearchResults
             data={keybindInfo}
             renderElement={(node) => (
               <Button
-                onClick={() => {
-                  node.identifier === selectedAction?.identifier
-                    ? setSelectedAction(null)
-                    : setSelectedAction(node);
-                }}
+                onClick={() => onClick(node)}
                 size={'md'}
                 variant={
                   node.identifier === selectedAction?.identifier ? 'filled' : 'light'
@@ -60,12 +84,7 @@ export function ListLayout() {
                 <Text truncate>{node.name}</Text>
               </Button>
             )}
-            matcherFunc={generateMatcherFunctionByKeys([
-              'name',
-              'key',
-              'modifiers',
-              'documentation'
-            ])}
+            matcherFunc={generateMatcherFunctionByKeys(['name', 'key', 'documentation'])}
           >
             <FilterList.SearchResults.VirtualList gap={'xs'} />
           </FilterList.SearchResults>
