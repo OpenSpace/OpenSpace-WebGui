3155fa170a93bc1eaf62b59ca755df87df271f86
Emma Broman <emma.broman@liu.se>
2025-02-28 17:33:06 +0100

    Add touch and focus support for inforbox tooltips

--- src/components/InfoBox/InfoBox.tsx
+++ src/components/InfoBox/InfoBox.tsx
@@ -9,8 +9,14 @@ interface Props {
 
 export function InfoBox({ text, w = 220 }: Props) {
   return (
-    <Tooltip label={text} multiline w={w} offset={{ mainAxis: 5, crossAxis: 100 }}>
-      <ThemeIcon radius={'xl'} size={'xs'}>
+    <Tooltip
+      label={text}
+      multiline
+      w={w}
+      offset={{ mainAxis: 5, crossAxis: 100 }}
+      events={{ hover: true, focus: true, touch: true }}
+    >
+      <ThemeIcon tabIndex={0} radius={'xl'} size={'xs'}>
         <InformationIcon />
       </ThemeIcon>
     </Tooltip>
