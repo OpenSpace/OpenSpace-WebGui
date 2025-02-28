bcdd536f7a03f245bc6c8381e0a46c54b9e7f0f8
Emma Broman <emma.broman@liu.se>
2025-02-28 18:23:05 +0100

    Refactor according to code review

--- src/components/Property/Types/ListProperty/ListProperty.tsx
+++ src/components/Property/Types/ListProperty/ListProperty.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { Pill, PillsInput } from '@mantine/core';
 
 import { PropertyLabel } from '@/components/Property/PropertyLabel';
@@ -14,7 +14,7 @@ export interface ListPropertyProps {
 }
 
 interface Props extends ListPropertyProps {
-  valueType: 'string' | 'int' | 'float';
+  valueType?: 'string' | 'int' | 'float';
 }
 
 export function ListProperty({
@@ -42,55 +42,51 @@ export function ListProperty({
     setShownValues(value);
   }, [value]);
 
-  // This needs to be a memoized function because it is used in a useEffect
-  const createPlaceHolderText = useCallback(
-    (values: ListValueType[]) => {
-      const count1 = values.length + 1;
-      const count2 = values.length + 2;
-      switch (valueType) {
-        case 'float':
-          return `number${count1}, number${count2}, ...`;
-        case 'int':
-          return `integer${count1}, integer${count2}, ...`;
-        case 'string':
-          return `item${count1}, item${count2}, ...`;
-        default:
-          throw new Error('Invalid value type');
-      }
-    },
-    [valueType]
-  );
+  const placeHolderText = useMemo(() => {
+    switch (valueType) {
+      case 'float':
+        return `number1, number2, ...`;
+      case 'int':
+        return `integer1, integer2, ...`;
+      case 'string':
+        return `item1, item2, ...`;
+      default:
+        throw new Error('Invalid value type');
+    }
+  }, [valueType]);
 
   useEffect(() => {
     const inputIsFocused = inputFieldRef.current === document.activeElement;
     if (inputIsFocused) {
-      setPlaceholderText(createPlaceHolderText(shownValues));
+      setPlaceholderText(placeHolderText);
     }
-  }, [createPlaceHolderText, shownValues]);
+  }, [placeHolderText]);
+
+  function parseInput(input: string): ListValueType[] {
+    const splitInput = input
+      .split(',')
+      .map((item) => item.trim())
+      .filter((item) => item !== '');
+
+    switch (valueType) {
+      case 'float':
+        return splitInput.map((item) => parseFloat(item)).filter((item) => !isNaN(item));
+      case 'int':
+        return splitInput.map((item) => parseInt(item)).filter((item) => !isNaN(item));
+      case 'string':
+        return splitInput;
+      default:
+        throw new Error('Invalid value type');
+    }
+  }
 
   function onInputKeyUp(event: React.KeyboardEvent<HTMLInputElement>) {
     const shouldEditOnBackSpace =
       (!isEditing || inputString.length === 0) && !(shownValues.length === 0);
 
     if (event.key === 'Enter') {
-      // Update values if enter is pressed
-      const splitInput = inputString
-        .split(',')
-        .map((item) => item.trim())
-        .filter((item) => item !== "");
-
-      let typedInput: ListValueType[] = splitInput;
-      if (valueType === 'float') {
-        typedInput = splitInput
-          .map((item) => parseFloat(item))
-          .filter((item) => !isNaN(item));
-      } else if (valueType === 'int') {
-        typedInput = splitInput
-          .map((item) => parseInt(item))
-          .filter((item) => !isNaN(item));
-      }
-
-      const newValues = [...shownValues, ...typedInput];
+      const inputValues = parseInput(inputString);
+      const newValues = [...shownValues, ...inputValues];
       setShownValues(newValues);
       setPropertyValue(newValues);
       stopEditing();
@@ -158,7 +154,7 @@ export function ListProperty({
           ref={inputFieldRef}
           placeholder={placeholderText}
           value={inputString}
-          onFocus={() => setPlaceholderText(createPlaceHolderText(shownValues))}
+          onFocus={() => setPlaceholderText(placeHolderText)}
           onBlur={() => setPlaceholderText('')}
           onChange={(event) => setInputString(event.currentTarget.value)}
           onKeyUp={(event) => onInputKeyUp(event)}
