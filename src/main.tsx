3c3bf37db75eb750f644e017e8929a362852ff4c
Ylva Selling <ylva.selling@gmail.com>
2025-02-28 17:28:11 +0100

    Add routes to gui and actions

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,34 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { MantineProvider } from '@mantine/core';
 
 import { LuaApiProvider } from './api/LuaApiProvider';
+import { ActionsPage } from './pages/ActionsPage.tsx';
+import { RoutesPage } from './pages/RoutesPage.tsx';
 import { store } from './redux/store';
+import { theme } from './theme/mantineTheme';
 import App from './App.tsx';
+
+import '@mantine/core/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
+    {/* We want to place the lua api provider here as it creates a socket connection.
+     We want this to be outside of the strict mode, which calls useEffects twice. 
+     The socket doesn't handle this well.*/}
     <LuaApiProvider>
       <StrictMode>
-        <App />
+        <MantineProvider theme={theme} defaultColorScheme={'dark'}>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<RoutesPage />} />
+              <Route path={'/frontend'} element={<App />} />
+              <Route path={'/actions'} element={<ActionsPage />} />
+            </Routes>
+          </BrowserRouter>
+        </MantineProvider>
       </StrictMode>
     </LuaApiProvider>
   </Provider>
