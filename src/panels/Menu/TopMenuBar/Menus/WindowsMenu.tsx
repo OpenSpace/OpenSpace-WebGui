f69de228d205241b03aa604efba996aa75b05739
Andreas Engberg <adde.engberg@gmail.com>
2025-03-11 10:51:34 +0100

    add correct items order in window menu

--- src/panels/Menu/TopMenuBar/Menus/WindowsMenu.tsx
+++ src/panels/Menu/TopMenuBar/Menus/WindowsMenu.tsx
@@ -4,30 +4,35 @@ import { IconSize } from '@/types/enums';
 import { menuItemsData } from '@/windowmanagement/data/MenuItems';
 import { useWindowLayoutProvider } from '@/windowmanagement/WindowLayout/hooks';
 
+import { useMenuItems } from '../../hooks';
 import { TopBarMenuWrapper } from '../TopBarMenuWrapper';
 
 export function WindowsMenu() {
   const { addWindow } = useWindowLayoutProvider();
+  const { menuItems } = useMenuItems();
 
   return (
     <TopBarMenuWrapper closeOnItemClick={false} targetTitle={'Windows'}>
       <Menu.Label>Add New Window</Menu.Label>
-      {Object.values(menuItemsData).map((item) => (
-        <Menu.Item
-          key={item.componentID}
-          leftSection={item.renderIcon?.(IconSize.xs)}
-          onClick={() => {
-            addWindow(item.content, {
-              title: item.title,
-              position: item.preferredPosition,
-              id: item.componentID,
-              floatPosition: item.floatPosition
-            });
-          }}
-        >
-          {item.title}
-        </Menu.Item>
-      ))}
+      {menuItems.map((itemConfig) => {
+        const item = menuItemsData[itemConfig.id];
+        return (
+          <Menu.Item
+            key={item.componentID}
+            leftSection={item.renderIcon?.(IconSize.xs)}
+            onClick={() => {
+              addWindow(item.content, {
+                title: item.title,
+                position: item.preferredPosition,
+                id: item.componentID,
+                floatPosition: item.floatPosition
+              });
+            }}
+          >
+            {item.title}
+          </Menu.Item>
+        );
+      })}
     </TopBarMenuWrapper>
   );
 }
