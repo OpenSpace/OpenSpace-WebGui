ede4d750a541ae7b6e8ffd0a0180c46128f4bacc
Ylva Selling <ylva.selling@gmail.com>
2025-03-12 13:25:09 +0100

    Feature/router (#76)
    * Install react router
    
    * Make window provider its own component
    
    * Don't have max no of columns for dynamic grid
    
    * Add two new pages
    
    * Add routes to gui and actions
    
    * Lint fix
    
    * Remove the documentation for now
    
    * Lazy load the actions panel for the actions page
    
    * Address comments on PR
    
    * Lint fix
    
    * Make image visible in firefox
    
    * Update src/App.tsx
    
    Co-authored-by: Emma Broman <emma.broman@liu.se>
    
    ---------
    
    Co-authored-by: Emma Broman <emma.broman@liu.se>

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,7 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 
-import { LuaApiProvider } from './api/LuaApiProvider';
-import { store } from './redux/store';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <LuaApiProvider>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </LuaApiProvider>
-  </Provider>
-);
+import '@mantine/core/styles.css';
+
+createRoot(document.getElementById('root')!).render(<App />);
---
+++ src/pages/RoutesPage.tsx
@@ -0,0 +1,47 @@
+import { AppShell, Group, Image, NavLink, Title } from '@mantine/core';
+
+import { DashboardIcon, HomeIcon } from '@/icons/icons';
+
+export function RoutesPage() {
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm'
+      }}
+      padding={'md'}
+    >
+      <AppShell.Header>
+        <Group h={'100%'} px={'md'}>
+          <Image
+            src={'openspace-logo.png'}
+            alt={'OpenSpace logo'}
+            h={30}
+            w={40}
+            fit={'contain'}
+          />
+          <Title>OpenSpace Routes</Title>
+        </Group>
+      </AppShell.Header>
+
+      <AppShell.Navbar p={'md'}>
+        <NavLink
+          href={'/frontend'}
+          label={'GUI'}
+          leftSection={<HomeIcon />}
+          description={'The regular OpenSpace GUI'}
+        />
+        <NavLink
+          href={'/actions'}
+          label={'Actions'}
+          description={'Actions panel opened up as a page'}
+          leftSection={<DashboardIcon />}
+        />
+      </AppShell.Navbar>
+      <AppShell.Main>
+        This page displays the different routes that are available in OpenSpace.
+      </AppShell.Main>
+    </AppShell>
+  );
+}
