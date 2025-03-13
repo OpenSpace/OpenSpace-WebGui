8258eb97ddd99bd743edc8dd143e6a2c6ef7e922
Ylva Selling <ylva.selling@gmail.com>
2025-03-13 07:56:16 +0100

    Make highlight of items be inside the object

--- src/theme/mantineTheme.ts
+++ src/theme/mantineTheme.ts
@@ -11,6 +11,7 @@ import {
   VariantColorsResolver
 } from '@mantine/core';
 
+import styles from './mantineTheme.module.css';
 /**
  * This functions resolves colors for different variants, and adds new variants, for
  * Mantine components.
@@ -96,5 +97,6 @@ export const theme = createTheme({
       h6: { fontSize: '0.75rem' }
     }
   },
+  focusClassName: styles.focus,
   variantColorResolver
 });
