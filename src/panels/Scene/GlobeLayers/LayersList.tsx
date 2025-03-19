556ae594fc33f43f0238858e6afc679b97cd64d6
Ylva Selling <ylva.selling@gmail.com>
2025-03-19 09:28:19 +0100

    Use component for layer reordering

--- src/panels/Scene/GlobeLayers/LayersList.tsx
+++ src/panels/Scene/GlobeLayers/LayersList.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
-import { DragDropContext, Draggable, Droppable, DropResult } from '@hello-pangea/dnd';
-import { Box, Group, ThemeIcon } from '@mantine/core';
+import { Box } from '@mantine/core';
 
 import { useOpenSpaceApi } from '@/api/hooks';
-import { DragHandleIcon } from '@/icons/icons';
+import {
+  DragReorderList,
+  OnDragEndProps
+} from '@/components/DragReorderList/DragReorderList';
 import { Identifier, Uri } from '@/types/types';
 
 import { GlobeLayer } from './GlobeLayer';
@@ -15,60 +16,25 @@ interface Props {
 }
 
 export function LayerList({ globe, layerGroup, layers }: Props) {
-  const [renderedLayersList, setRenderedLayersList] = useState(layers);
-
-  useEffect(() => {
-    setRenderedLayersList(layers);
-  }, [layers]);
-
   const luaApi = useOpenSpaceApi();
 
-  if (!renderedLayersList || renderedLayersList.length === 0) {
-    return <></>;
-  }
-
-  async function onDragEnd(result: DropResult<string>) {
-    if (!result.destination || result.source.index === result.destination.index) {
-      // No change - do nothing
-      return;
-    }
-    // Get new layer order
-    const updatedLayers = Array.from(renderedLayersList);
-    const [movedLayer] = updatedLayers.splice(result.source.index, 1);
-    updatedLayers.splice(result.destination.index, 0, movedLayer);
-    setRenderedLayersList(updatedLayers);
+  function onDragEnd({ oldIndex, newIndex }: OnDragEndProps<Uri>) {
     // Call the scripting for the engine
-    await luaApi?.globebrowsing.moveLayer(
-      globe,
-      layerGroup,
-      result.source.index,
-      result.destination.index
-    );
+    luaApi?.globebrowsing.moveLayer(globe, layerGroup, oldIndex, newIndex);
   }
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId={'layers'}>
-        {(provided) => (
-          <div {...provided.droppableProps} ref={provided.innerRef}>
-            {renderedLayersList.map((layerUri, index) => (
-              <Draggable key={layerUri} draggableId={layerUri} index={index}>
-                {(item) => (
-                  <Group ref={item.innerRef} gap={'xs'} {...item.draggableProps}>
-                    <Box flex={1}>
-                      <GlobeLayer uri={layerUri} />
-                    </Box>
-                    <ThemeIcon {...item.dragHandleProps}>
-                      <DragHandleIcon />
-                    </ThemeIcon>
-                  </Group>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <DragReorderList
+      onDragEnd={onDragEnd}
+      renderFunc={(id) => (
+        <Box flex={1}>
+          <GlobeLayer uri={id} />
+        </Box>
+      )}
+      data={layers}
+      keyFunc={(id) => id}
+      id={`${globe}${layerGroup}`}
+      dragHandlePosition={"right"}
+    />
   );
 }
