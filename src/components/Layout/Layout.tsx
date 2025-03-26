ba339a2b0bff57680ab2d9502205fa8ae68dfb94
Ylva Selling <ylva.selling@gmail.com>
2025-03-26 08:23:55 +0100

    Fix issue with jumpy scrollbar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,26 +1,14 @@
 import { PropsWithChildren } from 'react';
-import { Box, BoxProps } from '@mantine/core';
-import { useElementSize } from '@mantine/hooks';
+import { BoxProps, Flex } from '@mantine/core';
 
 import { FixedContent } from './FixedContent';
 import { GrowingContent } from './GrowingContent';
-import { LayoutContext } from './LayoutContext';
 
 export function Layout({ children, ...props }: BoxProps & PropsWithChildren) {
-  const { ref: parentRef, height: parentHeight } = useElementSize();
-  const { ref: fixedContentRef, height: fixedContentHeight } = useElementSize();
-
   return (
-    <LayoutContext.Provider
-      value={{
-        growingSizeHeight: parentHeight - fixedContentHeight,
-        ref: fixedContentRef
-      }}
-    >
-      <Box ref={parentRef} h={'100%'} {...props}>
-        {children}
-      </Box>
-    </LayoutContext.Provider>
+    <Flex direction={'column'} h={'100%'} wrap={'nowrap'} {...props}>
+      {children}
+    </Flex>
   );
 }
 
