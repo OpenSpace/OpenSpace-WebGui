e6e0ac3db22d603ab3baff988e11c36307d957e7
Ylva Selling <ylva.selling@gmail.com>
2025-03-27 07:55:59 +0100

    Address PR comments

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,9 @@
-import { PropsWithChildren } from 'react';
-import { BoxProps, Flex } from '@mantine/core';
+import { Flex, FlexProps } from '@mantine/core';
 
 import { FixedContent } from './FixedContent';
 import { GrowingContent } from './GrowingContent';
 
-export function Layout({ children, ...props }: BoxProps & PropsWithChildren) {
+export function Layout({ children, ...props }: FlexProps) {
   return (
     <Flex direction={'column'} h={'100%'} wrap={'nowrap'} {...props}>
       {children}
