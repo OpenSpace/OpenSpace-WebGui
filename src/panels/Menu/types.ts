e08fb9c8944508237f231f9386fc1e1686a731cb
Ylva Selling <ylva.selling@gmail.com>
2025-03-31 11:41:12 +0200

    Create a way to save a file that stores the bottom bar info (#90)
    * Create a way to save a file that stores the bottom bar info
    
    * Make layout.json be default file name
    
    * Switch to mantine dialog for loading a file
    
    * Create fallback save option for firefox
    
    * Lint fix
    
    * Prepare gui for the changes in the engine with the profile editor
    
    * Better comments
    
    * Even better comments
    
    * Make the file IO stuff a hook
    
    * Add profile topic
    
    * Remove profile slice and handle fulfilled profile topic in middleware
    
    * Address PR comments
    
    * Validate layout upon reading file
    
    * Update src/panels/Menu/hooks.ts
    
    Co-authored-by: Emma Broman <emma.broman@liu.se>
    
    * Address PR comments
    
    * Add console errors and TODO comments; address PR comments
    
    * Lint fix
    
    ---------
    
    Co-authored-by: Emma Broman <emma.broman@liu.se>

--- src/panels/Menu/types.ts
+++ src/panels/Menu/types.ts
@@ -1,5 +1,7 @@
 export interface TaskbarItemConfig {
   id: string;
+  // The name of the menu item. This will show up in the OpenSpace launcher.
+  name: string;
   // Whether the menu item should be visible in the taskbar.
   // @TODO anden88 2025-03-12: determine if we will be using the enabled property to
   // to disable using a certain menu, e.g., disable Sky browser in a dome.
