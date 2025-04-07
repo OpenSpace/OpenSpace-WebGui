c95a1328937e7c468cd2c33f2798598141952991
Ylva Selling <ylva.selling@gmail.com>
2025-04-07 10:57:51 +0200

    Add comment

--- src/panels/GettingStartedPanel/Steps/ButtonWrapper.tsx
+++ src/panels/GettingStartedPanel/Steps/ButtonWrapper.tsx
@@ -5,6 +5,9 @@ interface Props extends PropsWithChildren, MantineStyleProps {
   withBorder?: boolean;
 }
 
+// This component is designed to be used as a wrapper for buttons in the Getting Started Panel
+// for buttons that we only want to displat "as an image", i.e. show what they look like
+// without actually being clickable.
 export function ButtonWrapper({ withBorder = false, children, ...styleProps }: Props) {
   return (
     <Paper
