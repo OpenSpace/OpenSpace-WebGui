fcc24c576c0594ebeaddf092cecf648c66190624
Ylva Selling <ylva.selling@gmail.com>
2025-04-10 15:57:19 +0200

    Add pinching for zooming

--- src/panels/SkyBrowserPanel/WorldWideTelescope/WorldWideTelescopeView.tsx
+++ src/panels/SkyBrowserPanel/WorldWideTelescope/WorldWideTelescopeView.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
-import { Box, LoadingOverlay, Text } from '@mantine/core';
+import { LoadingOverlay, Text } from '@mantine/core';
 
 import { useOpenSpaceApi } from '@/api/hooks';
 import { useBoolProperty } from '@/hooks/properties';
 import { useAppSelector } from '@/redux/hooks';
 import { useWindowSize } from '@/windowmanagement/Window/hooks';
+import { useGesture } from '@use-gesture/react';
+import styles from './WorldWideTelescope.module.css';
 
 import { useWwtProvider } from './WwtProvider/hooks';
 import {
@@ -30,8 +32,36 @@ export function WorldWideTelescopeView() {
   const { width, height } = useWindowSize();
   const luaApi = useOpenSpaceApi();
 
+  const bind = useGesture(
+    {
+      onDrag: (state) => {
+        state.event.preventDefault();
+        if (!state.wheeling && !state.pinching && state.touches === 1) {
+          handleDrag(state.offset[0], state.offset[1]);
+        }
+      },
+      onDragStart: (state) => {
+        if (state.touches === 1 && !state.wheeling && !state.pinching) {
+          mouseDown(state.offset[0], state.offset[1]);
+        }
+      },
+      onDragEnd: () => mouseUp(),
+      onPinch: (state) => {
+        if (state.touches == 2) {
+          const direction = state.direction[0];
+          scroll(direction < 0 ? 50 : -50);
+        }
+      },
+      onWheel: (state) => {
+        scroll(state.event.deltaY);
+      }
+    },
+    {
+      drag: { threshold: 0.1 }, // To make it differentiate better between dragging and pinching
+      eventOptions: { passive: false }
+    }
+  );
   const [inverseZoom] = useBoolProperty('Modules.SkyBrowser.InverseZoomDirection');
-
   // A bunch of hooks that pass messages to WWT when our redux state changes
   useUpdateAim(id);
   useUpdateSelectedImages(id);
@@ -76,6 +106,7 @@ export function WorldWideTelescopeView() {
     if (!id) {
       return;
     }
+    //console.log(deltaY);
     const scrollDirection = inverseZoom ? -deltaY : deltaY;
     luaApi?.skybrowser.scrollOverBrowser(id, scrollDirection);
     luaApi?.skybrowser.stopAnimations(id);
@@ -104,21 +135,16 @@ export function WorldWideTelescopeView() {
         }}
         transitionProps={{ transition: 'fade', duration: 500 }}
       />
-      <Box
-        component={'button'}
-        onMouseMove={(e) => handleDrag(e.clientX, e.clientY)}
-        onMouseDown={(e) => mouseDown(e.clientX, e.clientY)}
-        onMouseUp={mouseUp}
-        onMouseLeave={mouseUp}
-        onWheel={(e) => scroll(e.deltaY)}
+      <div
         aria-label={'Dragging area for WorldWideTelescope'}
-        p={'0px'}
-        bg={'transparent'}
-        pos={'absolute'}
-        bd={'0px'}
         style={{
-          cursor: isDragging ? 'grabbing' : 'grab'
+          cursor: isDragging ? 'grabbing' : 'grab',
+          padding: '0px',
+          margin: '0px',
+          position: 'absolute'
         }}
+        className={styles.dragArea}
+        {...bind()}
       >
         <iframe
           ref={ref}
@@ -138,7 +164,8 @@ export function WorldWideTelescopeView() {
         >
           <p>ERROR: cannot display AAS WorldWide Telescope research app!</p>
         </iframe>
-      </Box>
+        {/* </Box> */}
+      </div>
     </>
   );
 }
