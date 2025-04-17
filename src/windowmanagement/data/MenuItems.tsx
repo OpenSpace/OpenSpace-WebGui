fe499fffb1a61003d87b0352fe7eb307cb6b6019
Ylva Selling <ylva.selling@gmail.com>
2025-04-17 13:19:45 +0200

    Fix typo

--- src/windowmanagement/data/MenuItems.tsx
+++ src/windowmanagement/data/MenuItems.tsx
@@ -175,7 +175,7 @@ export const menuItemsData: Record<string, MenuItem> = {
     content: <FlightControlPanel />,
     renderIcon: (size) => <ExpandArrowsIcon size={size} />,
     preferredPosition: 'right',
-    defaultVisible: !window.IsWithinCEF
+    defaultVisible: !window.isWithinCEF
   },
   keybindingsLayout: {
     title: 'Keybindings Layout',
