6b175ee3beb7bb6238d753ed8406853da82314f0
Emma Broman <emma.broman@liu.se>
2025-04-28 10:32:14 +0200

    Address review comments and add some padding

--- src/panels/DevPanel/DevPanel.tsx
+++ src/panels/DevPanel/DevPanel.tsx
@@ -1,16 +1,19 @@
-import { Tabs } from '@mantine/core';
+import { Box, Tabs } from '@mantine/core';
 
 import { PropertyTest } from './Subpanels/PropertyTest';
 
 export function DevPanel() {
   return (
-    <Tabs allowTabDeactivation>
+    <Tabs defaultValue={'propertyTest'}>
       <Tabs.List>
         <Tabs.Tab value={'propertyTest'}>Property Test</Tabs.Tab>
       </Tabs.List>
-      <Tabs.Panel value={'propertyTest'}>
-        <PropertyTest />
-      </Tabs.Panel>
+
+      <Box pt={'md'}>
+        <Tabs.Panel value={'propertyTest'}>
+          <PropertyTest />
+        </Tabs.Panel>
+      </Box>
     </Tabs>
   );
 }
--- src/panels/DevPanel/Subpanels/PropertyTest.tsx
+++ src/panels/DevPanel/Subpanels/PropertyTest.tsx
@@ -1,4 +1,4 @@
-import { Container, Divider, ScrollArea } from '@mantine/core';
+import { Container, Title } from '@mantine/core';
 
 import { Property } from '@/components/Property/Property';
 import { PropertyOwner } from '@/components/PropertyOwner/PropertyOwner';
@@ -8,54 +8,64 @@ import { PropertyOwner } from '@/components/PropertyOwner/PropertyOwner';
  */
 export function PropertyTest() {
   return (
-    <ScrollArea h={'100%'}>
-      <Container>
-        <Divider />
-        <h3>Bool (Earth):</h3>
-        <Property uri={'Scene.Earth.Renderable.Enabled'} />
-        <h3>String:</h3>
-        <Property uri={'Scene.Earth.Renderable.Type'} />
-        <Property uri={'Scene.Earth.Renderable.LightSourceNode'} />
-        <h3>Trigger:</h3>
-        <Property uri={'Scene.Earth.Renderable.Layers.ColorLayers.Blue_Marble.Reset'} />
-        <h3>Option:</h3>
-        <Property
-          uri={'Scene.Earth.Renderable.Layers.ColorLayers.ESRI_VIIRS_Combo.BlendMode'}
-        />
-        <h3>Selection:</h3>
-        <Property uri={'Scene.Constellations.Renderable.ConstellationSelection'} />
-        <h3>String Lists:</h3>
-        <Property
-          uri={'Modules.Server.Interfaces.DefaultTcpSocketInterface.DenyAddresses'}
-        />
-        <h3>Int / Double Lists:</h3>
-        <Property uri={'RenderEngine.ScreenshotWindowId'} />
-        <h3>Numeric Property</h3>
-        <h4>"Nice" value (Earth opacity):</h4>
-        <Property uri={'Scene.Earth.Renderable.Opacity'} />
-        <h4>Disabled:</h4>
-        <Property uri={'Scene.Earth.Renderable.NActiveLayers'} />
-        <h4>Exponential:</h4>
-        <Property uri={'Scene.Earth.ApproachFactor'} />
-        <h4>Linear, but not nice values:</h4>
-        <Property uri={'Scene.Earth.BoundingSphere'} />
-        <h4>Int:</h4>
-        <Property uri={'Scene.EclipticLine.Renderable.CircleSegments'} />
+    <Container>
+      <Title order={3}>Bool (Earth):</Title>
+      <Property uri={'Scene.Earth.Renderable.Enabled'} />
 
-        <h3>Vector Property:</h3>
-        <h4>Colors</h4>
+      <Title order={3}>String:</Title>
+      <Property uri={'Scene.Earth.Renderable.Type'} />
+      <Property uri={'Scene.Earth.Renderable.LightSourceNode'} />
+
+      <Title order={3}>Trigger:</Title>
+      <Property uri={'Scene.Earth.Renderable.Layers.ColorLayers.Blue_Marble.Reset'} />
+
+      <Title order={3}>Option:</Title>
+      <Property
+        uri={'Scene.Earth.Renderable.Layers.ColorLayers.ESRI_VIIRS_Combo.BlendMode'}
+      />
+
+      <Title order={3}>Selection:</Title>
+      <Property uri={'Scene.Constellations.Renderable.ConstellationSelection'} />
+
+      <Title order={3}>String Lists:</Title>
+      <Property
+        uri={'Modules.Server.Interfaces.DefaultTcpSocketInterface.DenyAddresses'}
+      />
+
+      <Title order={3}>Int / Double Lists:</Title>
+      <Property uri={'RenderEngine.ScreenshotWindowId'} />
+
+      <Title order={3}>Numeric Property:</Title>
+      <Container>
+        <Title order={4}>"Nice" value (Earth opacity):</Title>
+        <Property uri={'Scene.Earth.Renderable.Opacity'} />
+        <Title order={4}>Disabled:</Title>
+        <Property uri={'Scene.Earth.Renderable.NActiveLayers'} />
+        <Title order={4}>Exponential:</Title>
+        <Property uri={'Scene.Earth.ApproachFactor'} />
+        <Title order={4}>Linear, but not nice values:</Title>
+        <Property uri={'Scene.Earth.BoundingSphere'} />
+        <Title order={4}>Int:</Title>
+        <Property uri={'Scene.EclipticLine.Renderable.CircleSegments'} />
+      </Container>
+
+      <Title order={3}>Vector Property:</Title>
+      <Container>
+        <Title order={4}>Colors:</Title>
         <Property uri={'Scene.1ldGrid.Renderable.Color'} />
         <Property uri={'RenderEngine.EnabledFontColor'} />
-        <h4>IntVec2</h4>
+        <Title order={4}>IntVec2:</Title>
         <Property uri={'Scene.1ldGrid.Renderable.Segments'} />
-        <h4>MinMaxRange</h4>
+        <Title order={4}>MinMaxRange:</Title>
+        TODO: More variants
         <Property uri={'Scene.SloanDigitalSkySurvey.Renderable.Fading.FadeInDistances'} />
-        <h4>TODO: More variants</h4>
-        <h3>Matrix Property:</h3>
-        <Property uri={'Scene.ISS.Renderable.ModelTransform'} />
-        <h3>A full property owner!</h3>
-        <PropertyOwner uri={'Scene.Earth'} />
       </Container>
-    </ScrollArea>
+
+      <Title order={3}>Matrix Property:</Title>
+      <Property uri={'Scene.ISS.Renderable.ModelTransform'} />
+
+      <Title order={3}>A full property owner:</Title>
+      <PropertyOwner uri={'Scene.Earth'} />
+    </Container>
   );
 }
