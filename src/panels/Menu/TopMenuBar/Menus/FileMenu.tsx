da2b2f94bbfb0844989a0516667b4b22aa5bcbe2
Emma Broman <emma.broman@liu.se>
2025-04-30 11:53:10 +0200

    Move the profile name from top bar to file menu, and add more info

--- src/panels/Menu/TopMenuBar/Menus/FileMenu.tsx
+++ src/panels/Menu/TopMenuBar/Menus/FileMenu.tsx
@@ -1,13 +1,18 @@
-import { Menu, Text } from '@mantine/core';
+import { Anchor, Group, Menu, Text } from '@mantine/core';
 import { modals } from '@mantine/modals';
 
 import { useOpenSpaceApi } from '@/api/hooks';
+import { InfoBox } from '@/components/InfoBox/InfoBox';
+import { LoadingBlocks } from '@/components/LoadingBlocks/LoadingBlocks';
 import { useProperty } from '@/hooks/properties';
 import { ConsoleIcon, ExitAppIcon } from '@/icons/icons';
+import { useAppSelector } from '@/redux/hooks';
 
 import { TopBarMenuWrapper } from '../TopBarMenuWrapper';
 
 export function FileMenu() {
+  const profile = useAppSelector((state) => state.profile);
+
   const luaApi = useOpenSpaceApi();
 
   const [isConsoleVisible, setIsConsoleVisible] = useProperty(
@@ -30,7 +35,38 @@ export function FileMenu() {
   }
 
   return (
-    <TopBarMenuWrapper targetTitle={'File'}>
+    <TopBarMenuWrapper targetTitle={'File'} closeOnItemClick={false}>
+      <Menu.Label>
+        {!profile.initalized ? (
+          <LoadingBlocks n={1} />
+        ) : (
+          <Group justify={'space-between'} align={'center'}>
+            Profile: {profile.name}
+            <InfoBox>
+              <Text size={'xs'} mb={'xs'} style={{ wordBreak: 'break-word' }}>
+                {profile.filePath}
+              </Text>
+              {profile.description && (
+                <Text size={'sm'} mb={'xs'}>
+                  {profile.description}
+                </Text>
+              )}
+              {profile.author && <Text size={'sm'}>Author: {profile.author}</Text>}
+              {profile.license && <Text size={'sm'}>License: {profile.license}</Text>}
+              {profile.url && (
+                <Text size={'sm'}>
+                  URL:{' '}
+                  <Anchor href={profile.url} target={'_blank'}>
+                    {profile.url}
+                  </Anchor>
+                </Text>
+              )}
+            </InfoBox>
+          </Group>
+        )}
+      </Menu.Label>
+
+      <Menu.Divider />
       <Menu.Item onClick={toggleLuaConsole} leftSection={<ConsoleIcon />}>
         Toggle Console
       </Menu.Item>
--- src/panels/Menu/TopMenuBar/TopMenuBar.tsx
+++ src/panels/Menu/TopMenuBar/TopMenuBar.tsx
@@ -1,10 +1,9 @@
-import { alpha, Box, Flex, Group, Text } from '@mantine/core';
+import { alpha, Box, Flex, Group } from '@mantine/core';
 
 import { FrictionControls } from '@/components/FrictionControls/FrictionControls';
 import { FrictionControlsInfo } from '@/components/FrictionControls/FrictionControlsInfo';
 import { InfoBox } from '@/components/InfoBox/InfoBox';
 import { ScrollBox } from '@/components/ScrollBox/ScrollBox';
-import { useAppSelector } from '@/redux/hooks';
 
 import { FileMenu } from './Menus/FileMenu';
 import { HelpMenu } from './Menus/HelpMenu';
@@ -13,8 +12,6 @@ import { WindowsMenu } from './Menus/WindowsMenu';
 // import { LocaleSwitcher } from './Menus/LocaleSwitcher';
 
 export function TopMenuBar() {
-  const name = useAppSelector((state) => state.profile.name);
-
   return (
     <ScrollBox direction={'horizontal'}>
       <Flex
@@ -35,7 +32,7 @@ export function TopMenuBar() {
           {/* <LocaleSwitcher /> */}
         </Group>
         <Box flex={1} style={{ overflow: 'hidden' }}>
-          <Text ta={'center'}>Profile: {name}</Text>
+          {/* There's space to put something in the center here if we want */}
         </Box>
         <Group flex={1} wrap={'nowrap'} align={'center'} justify={'end'} mr={'xs'}>
           <FrictionControls size={'xs'} />
