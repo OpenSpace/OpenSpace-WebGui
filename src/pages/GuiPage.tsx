bb7b51e3ad9371eb98c59a4a5bfc8cba14faee21
Andreas Engberg <adde.engberg@gmail.com>
2025-05-05 08:25:50 +0200

    lint

--- src/pages/GuiPage.tsx
+++ src/pages/GuiPage.tsx
@@ -1,14 +1,14 @@
 import { ErrorBoundary } from 'react-error-boundary';
 import { ModalsProvider } from '@mantine/modals';
 
-import { fallbackRender } from '@/components/ErrorFallback/fallbackRender';
+import { FallbackRender } from '@/components/ErrorFallback/fallbackRender';
 import { WindowLayout } from '@/windowmanagement/WindowLayout/WindowLayout';
 import { WindowLayoutProvider } from '@/windowmanagement/WindowLayout/WindowLayoutProvider';
 
 export function GuiPage() {
   return (
     <ErrorBoundary
-      fallbackRender={fallbackRender}
+      fallbackRender={FallbackRender}
       onReset={() => window.location.reload()}
     >
       <ModalsProvider>
