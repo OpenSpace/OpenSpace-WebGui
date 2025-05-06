f722de407a4890c72ee8254fb0df2db2be31f7ad
Emma Broman <emma.broman@liu.se>
2025-05-06 15:38:08 +0200

    Move profile name from top bar center to File menu (#133)
    * Move the profile name from top bar to file menu, and add more info
    
    * Make the profile info a menu piece instead
    
    * Restructure code for loading state

--- src/panels/Menu/TopMenuBar/Menus/FileMenu.tsx
+++ src/panels/Menu/TopMenuBar/Menus/FileMenu.tsx
@@ -1,13 +1,19 @@
-import { Menu, Text } from '@mantine/core';
+import { Anchor, Container, Group, Menu, Text } from '@mantine/core';
 import { modals } from '@mantine/modals';
 
 import { useOpenSpaceApi } from '@/api/hooks';
+import { InfoBox } from '@/components/InfoBox/InfoBox';
+import { LoadingBlocks } from '@/components/LoadingBlocks/LoadingBlocks';
 import { useProperty } from '@/hooks/properties';
-import { ConsoleIcon, ExitAppIcon } from '@/icons/icons';
+import { ChevronRightIcon, ConsoleIcon, ExitAppIcon } from '@/icons/icons';
+import { useAppSelector } from '@/redux/hooks';
+import { IconSize } from '@/types/enums';
 
 import { TopBarMenuWrapper } from '../TopBarMenuWrapper';
 
 export function FileMenu() {
+  const profile = useAppSelector((state) => state.profile);
+
   const luaApi = useOpenSpaceApi();
 
   const [isConsoleVisible, setIsConsoleVisible] = useProperty(
@@ -30,7 +36,48 @@ export function FileMenu() {
   }
 
   return (
-    <TopBarMenuWrapper targetTitle={'File'}>
+    <TopBarMenuWrapper targetTitle={'File'} closeOnItemClick={false}>
+      {!profile.initalized ? (
+        <LoadingBlocks n={1} />
+      ) : (
+        <>
+          <Menu.Label>Profile: {profile.name}</Menu.Label>
+          <TopBarMenuWrapper
+            targetTitle={
+              <Menu.Item rightSection={<ChevronRightIcon size={IconSize.sm} />}>
+                About
+              </Menu.Item>
+            }
+            position={'right-start'}
+            withinPortal={false}
+            closeOnItemClick={false}
+          >
+            <Container py={'xs'} px={'xs'} maw={300}>
+              <Text>{profile.name}</Text>
+              <Menu.Divider />
+              {profile.description && (
+                <Text size={'sm'} mb={'xs'}>
+                  {profile.description}
+                </Text>
+              )}
+              {profile.author && <Text size={'sm'}>Author: {profile.author}</Text>}
+              {profile.license && <Text size={'sm'}>License: {profile.license}</Text>}
+              {profile.url && (
+                <Text size={'sm'}>
+                  URL:{' '}
+                  <Anchor href={profile.url} target={'_blank'}>
+                    {profile.url}
+                  </Anchor>
+                </Text>
+              )}
+              <Text size={'xs'} mt={'xs'} style={{ wordBreak: 'break-word' }}>
+                {profile.filePath}
+              </Text>
+            </Container>
+          </TopBarMenuWrapper>
+        </>
+      )}
+      <Menu.Divider />
       <Menu.Item onClick={toggleLuaConsole} leftSection={<ConsoleIcon />}>
         Toggle Console
       </Menu.Item>
