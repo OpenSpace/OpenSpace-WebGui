401c95b807313192af860fc26fb68a3a10ee8b48
Emma Broman <emma.broman@liu.se>
2025-05-08 09:55:59 +0200

    Fix wrapping for identifier and add copy button

--- src/panels/KeybindsPanel/KeybindInfo.tsx
+++ src/panels/KeybindsPanel/KeybindInfo.tsx
@@ -1,10 +1,12 @@
-import { Badge, Code, Paper, Table, Title } from '@mantine/core';
+import { Badge, Code, Group, Paper, Table, Title } from '@mantine/core';
 
+import { CopyToClipboardButton } from '@/components/CopyToClipboardButton/CopyToClipboardButton';
 import { Action } from '@/types/types';
 
 interface Props {
   selectedAction: Action;
 }
+
 export function KeybindInfo({ selectedAction }: Props) {
   return (
     <Paper key={selectedAction.identifier} p={'md'}>
@@ -36,7 +38,12 @@ export function KeybindInfo({ selectedAction }: Props) {
           <Table.Tr>
             <Table.Td>Identifier:</Table.Td>
             <Table.Td>
-              <Code>{selectedAction.identifier}</Code>
+              <Group gap={'xs'} wrap={'nowrap'}>
+                <Code style={{ wordBreak: 'break-word' }}>
+                  {selectedAction.identifier}
+                </Code>
+                <CopyToClipboardButton value={selectedAction.identifier} />
+              </Group>
             </Table.Td>
           </Table.Tr>
         </Table.Tbody>
