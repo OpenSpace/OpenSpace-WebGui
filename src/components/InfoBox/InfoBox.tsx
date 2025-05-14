98a53e2e3e873150f711220d715675e1387739fd
Andreas Engberg <48772850+engbergandreas@users.noreply.github.com>
2025-05-14 11:56:34 +0200

    Language/components (#134)
    * add typescript intellisense and highlighting
    
    * add localization for components

--- src/components/InfoBox/InfoBox.tsx
+++ src/components/InfoBox/InfoBox.tsx
@@ -1,4 +1,5 @@
 import { PropsWithChildren, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { ActionIcon, Popover } from '@mantine/core';
 
 import { InformationIcon } from '@/icons/icons';
@@ -9,6 +10,7 @@ interface Props {
 
 export function InfoBox({ children, w = 320 }: Props & PropsWithChildren) {
   const [opened, setOpened] = useState(false);
+  const { t } = useTranslation('components', { keyPrefix: 'info-box' });
 
   return (
     <Popover
@@ -23,7 +25,7 @@ export function InfoBox({ children, w = 320 }: Props & PropsWithChildren) {
         <ActionIcon
           radius={'xl'}
           size={'xs'}
-          aria-label={'More information'}
+          aria-label={t('aria-label')}
           onClick={() => setOpened(!opened)}
         >
           <InformationIcon />
