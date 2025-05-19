7a61c844432e51ee4761855dcdc6469b935bb9ae
Andreas Engberg <adde.engberg@gmail.com>
2025-05-19 16:35:56 +0200

    fix issue with translation file not loading correctly in production

--- src/localization/config.tsx
+++ src/localization/config.tsx
@@ -12,6 +12,9 @@ export const SupportedLanguages: Record<string, LanguageInfo> = {
   sv: { language: 'Swedish', icon: <SE width={20} /> }
 };
 
+// @TODO anden88 2025-05-19: Temporary fix to get translation lookup working correctly
+const basePath = process.env.NODE_ENV === 'production' ? '/frontend/' : '/';
+
 i18n
   .use(HttpApi)
   .use(initReactI18next)
@@ -27,6 +30,9 @@ i18n
     interpolation: {
       // React already escapes any code in translation messages, safguarding against XSS
       escapeValue: false
+    },
+    backend: {
+      loadPath: `${basePath}locales/{{lng}}/{{ns}}.json`
     }
   });
 
