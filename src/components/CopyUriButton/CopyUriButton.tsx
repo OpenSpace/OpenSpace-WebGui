2b1e0088f4b18b90e439a83113003e3166faae3d
Emma Broman <emma.broman@liu.se>
2025-05-22 16:32:08 +0200

    Address code review comments

--- src/components/CopyUriButton/CopyUriButton.tsx
+++ src/components/CopyUriButton/CopyUriButton.tsx
@@ -1,5 +1,4 @@
-import { useTranslation } from 'react-i18next';
-import { Code, Group, Stack, Text } from '@mantine/core';
+import { Code, Group } from '@mantine/core';
 
 import { CopyToClipboardButton } from '../CopyToClipboardButton/CopyToClipboardButton';
 
@@ -8,20 +7,15 @@ interface Props {
 }
 
 function CopyUriButton({ uri }: Props) {
-  const { t } = useTranslation('components', { keyPrefix: 'copy-uri-button' });
-
   if (!uri) {
     return <></>;
   }
 
   return (
-    <Stack pt={'sm'}>
-      <Group gap={'xs'} wrap={'nowrap'}>
-        <Text size={'xs'}>{t('label')}:</Text>
-        <Code>{uri}</Code>
-        <CopyToClipboardButton value={uri} />
-      </Group>
-    </Stack>
+    <Group pt={'sm'} gap={'xs'} wrap={'nowrap'}>
+      <Code>{uri}</Code>
+      <CopyToClipboardButton value={uri} />
+    </Group>
   );
 }
 
