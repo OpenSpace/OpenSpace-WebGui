7c9ffa308c2a5ac3a1c547ebbb7a91140dd647ea
Ylva Selling <ylva.selling@gmail.com>
2025-06-09 13:45:47 +0200

    Make initial value in profile slice undefined

--- src/redux/profile/profileSlice.ts
+++ src/redux/profile/profileSlice.ts
@@ -5,7 +5,7 @@ import { Identifier } from '@/types/types';
 export interface ProfileState {
   initalized: boolean;
   uiPanelVisibility: Record<string, boolean>;
-  hasStartedBefore: boolean;
+  hasStartedBefore: boolean | undefined;
   markNodes: Identifier[];
   name: string | undefined;
   author: string | undefined;
@@ -19,7 +19,7 @@ export interface ProfileState {
 const initialState: ProfileState = {
   initalized: false,
   uiPanelVisibility: {},
-  hasStartedBefore: false,
+  hasStartedBefore: undefined,
   markNodes: [],
   name: undefined,
   author: undefined,
