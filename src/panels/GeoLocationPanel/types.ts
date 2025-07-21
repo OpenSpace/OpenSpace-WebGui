8866080987f913d9b5b0b7878a448179744f1dd9
Alexander Bock <alexander.bock@liu.se>
2025-07-21 16:39:37 +0200

    Add documentation for units in the GeoCoordinates

--- src/panels/GeoLocationPanel/types.ts
+++ src/panels/GeoLocationPanel/types.ts
@@ -6,7 +6,10 @@ export type MouseMarkerPosition =
   | undefined;
 
 export type GeoCoordinates = {
+  // Provided in decimal degrees in the range [-90, 90]
   lat: number;
+  // Provided in decimal degress in the range [-180, 180]
   long: number;
+  // Provided in km
   alt: number;
 };
