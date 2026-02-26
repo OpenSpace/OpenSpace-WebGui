d84ad667704e1254c8b3f04eb7b6adfd4b6d8db6
Micah Acinapura <micahnyc@gmail.com>
2026-02-26 12:07:00 -0500

    removing unnecessary grid

--- src/components/QRCode/QRCode.tsx
+++ src/components/QRCode/QRCode.tsx
@@ -25,59 +25,55 @@ export function QRCode({ opened, close }: Props) {
       size={'xl'}
       closeButtonProps={{ 'aria-label': t('close-button-aria-label') }}
     >
-      <Grid>
-        <Grid.Col span={12}>
-          <Stack gap={'xs'} align={'center'}>
-            <Title order={1}>{t('modal-title')}</Title>
-            <QRCodeSVG
-              value={`http://${address}:${port}/gui/#/routes/`}
-              title={t('alt-text')}
-              size={192}
-              bgColor={'var(--mantine-color-black)'}
-              fgColor={'var(--mantine-color-white)'}
-              level={'L'}
-              imageSettings={{
-                src: `${import.meta.env.BASE_URL}/images/icon.png`,
-                x: undefined,
-                y: undefined,
-                height: 24,
-                width: 24,
-                opacity: 1,
-                excavate: true
-              }}
-            />
-            <Text p={'md'} className={styles.selectable}>
-              {t('panel-description')}
-            </Text>
+        <Stack gap={'xs'} align={'center'}>
+          <Title order={1}>{t('modal-title')}</Title>
+          <QRCodeSVG
+            value={`http://${address}:${port}/gui/#/routes/`}
+            title={t('alt-text')}
+            size={192}
+            bgColor={'var(--mantine-color-black)'}
+            fgColor={'var(--mantine-color-white)'}
+            level={'L'}
+            imageSettings={{
+              src: `${import.meta.env.BASE_URL}/images/icon.png`,
+              x: undefined,
+              y: undefined,
+              height: 24,
+              width: 24,
+              opacity: 1,
+              excavate: true
+            }}
+          />
+          <Text p={'md'} className={styles.selectable}>
+            {t('panel-description')}
+          </Text>
 
-            <Alert variant={'light'} color={'blue'} title={t('ip-warning-title')}>
-              <Text pb={'md'}>
-                {t('ip-warning-text-1')}
-              </Text>
-              <Text pb={'md'} className={styles.selectable}>
-                {t('ip-warning-text-2')}
-              </Text>
-              <Text pb={'md'}>
-                {t('ip-warning-text-3')}
-              </Text>
-              <Divider my={'sm'} />
-              <Text pb={'sm'}>{t('current-address-value')}</Text>
-              <StringProperty uri={'Modules.WebGui.Address'} readOnly={true} />
-            </Alert>
-            <Alert variant={'light'} color={'blue'} title={t('allow-warning-title')}>
-              <Text pb={'md'} className={styles.selectable}>
-                {t('allow-warning-text')}
-              </Text>
-              <Property
-                uri={'Modules.Server.Interfaces.DefaultWebSocketInterface.DefaultAccess'}
-              />
-              <Property
-                uri={'Modules.Server.Interfaces.DefaultWebSocketInterface.AllowAddresses'}
-              />
-            </Alert>
-          </Stack>
-        </Grid.Col>
-      </Grid>
+          <Alert variant={'light'} color={'blue'} title={t('ip-warning-title')}>
+            <Text pb={'md'}>
+              {t('ip-warning-text-1')}
+            </Text>
+            <Text pb={'md'} className={styles.selectable}>
+              {t('ip-warning-text-2')}
+            </Text>
+            <Text pb={'md'}>
+              {t('ip-warning-text-3')}
+            </Text>
+            <Divider my={'sm'} />
+            <Text pb={'sm'}>{t('current-address-value')}</Text>
+            <StringProperty uri={'Modules.WebGui.Address'} readOnly={true} />
+          </Alert>
+          <Alert variant={'light'} color={'blue'} title={t('allow-warning-title')}>
+            <Text pb={'md'} className={styles.selectable}>
+              {t('allow-warning-text')}
+            </Text>
+            <Property
+              uri={'Modules.Server.Interfaces.DefaultWebSocketInterface.DefaultAccess'}
+            />
+            <Property
+              uri={'Modules.Server.Interfaces.DefaultWebSocketInterface.AllowAddresses'}
+            />
+          </Alert>
+        </Stack>
     </Modal>
   );
 }
