91645a43b61ae4cfbc9ec46173b5ef28b99e591c
Alexander Bock <alexander.bock@liu.se>
2026-06-24 16:11:32 +0200

    Add custom fly-to slider

--- src/panels/NavigationPanel/NavigationSettings.tsx
+++ src/panels/NavigationPanel/NavigationSettings.tsx
@@ -1,11 +1,25 @@
 import { useTranslation } from 'react-i18next';
-import { Container, Divider, Menu } from '@mantine/core';
+import {
+  Box,
+  Checkbox,
+  Container,
+  Divider,
+  Group,
+  Menu,
+  NumberInput,
+  Stack
+} from '@mantine/core';
 
 import { BoolInput } from '@/components/Input/BoolInput';
+import { NumericSlider } from '@/components/Input/NumericInput/NumericSlider/NumericSlider';
 import { Property } from '@/components/Property/Property';
 import { SettingsPopout } from '@/components/SettingsPopout/SettingsPopout';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import { setOnlyFocusableInNavMenu } from '@/redux/local/localSlice';
+import {
+  setFlyToOverrideDuration,
+  setFlyToOverrideDurationEnabled,
+  setOnlyFocusableInNavMenu
+} from '@/redux/local/localSlice';
 import {
   ApplyIdleMotionOnPathFinishKey,
   CameraPathArrivalDistanceFactorKey,
@@ -18,6 +32,10 @@ export function NavigationSettings() {
 
   const showOnlyFocusableInSearch = useAppSelector(
     (state) => state.local.menus.navigation.onlyFocusable
+  );
+
+  const flyToOverrideDuration = useAppSelector(
+    (state) => state.local.menus.navigation.flyToOverrideDuration
   );
 
   const dispatch = useAppDispatch();
@@ -40,6 +58,40 @@ export function NavigationSettings() {
         <Property uri={CameraPathSpeedFactorKey} />
         <Property uri={CameraPathArrivalDistanceFactorKey} />
         <Property uri={ApplyIdleMotionOnPathFinishKey} />
+        <Stack gap={'xs'} mt={'xs'}>
+          <Checkbox
+            label={t('override-fly-to-duration-label')}
+            checked={flyToOverrideDuration.enabled}
+            onChange={(event) =>
+              dispatch(setFlyToOverrideDurationEnabled(event.currentTarget.checked))
+            }
+          />
+          <Group gap={'xs'} wrap={'nowrap'} align={'center'}>
+            <Box style={{ flex: 1 }}>
+              <NumericSlider
+                min={0}
+                max={60}
+                step={0.25}
+                value={flyToOverrideDuration.duration}
+                disabled={!flyToOverrideDuration.enabled}
+                onInput={(value) => dispatch(setFlyToOverrideDuration(value))}
+              />
+            </Box>
+            <NumberInput
+              size={'xs'}
+              w={70}
+              min={0}
+              max={60}
+              step={1.25}
+              suffix={'s'}
+              disabled={!flyToOverrideDuration.enabled}
+              value={flyToOverrideDuration.duration}
+              onChange={(value) =>
+                dispatch(setFlyToOverrideDuration(typeof value === 'number' ? value : 0))
+              }
+            />
+          </Group>
+        </Stack>
       </Container>
     </SettingsPopout>
   );
