df08e3028cb44c07a72ae8e157a94bba64ea041f
Emma Broman <emma.broman@liu.se>
2026-08-14 13:24:50 +0200

    Make list of renderables a filterlist

--- src/panels/ScreenSpaceRenderablePanel/ScreenSpaceRenderablePanel.tsx
+++ src/panels/ScreenSpaceRenderablePanel/ScreenSpaceRenderablePanel.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Box, Divider, Group, Text, Title } from '@mantine/core';
+import { Box, Group, Stack, Text, ThemeIcon } from '@mantine/core';
 
+import { FilterList } from '@/components/FilterList/FilterList';
 import { ResizeableContent } from '@/components/ResizeableContent/ResizeableContent';
-import { ScrollBox } from '@/components/ScrollBox/ScrollBox';
 import { usePropertyOwner } from '@/hooks/propertyOwner';
+import { InsertPhotoIcon } from '@/icons/icons';
+import { useAppSelector } from '@/redux/hooks';
+import { propertyOwnerSelectors } from '@/redux/propertytree/propertyOwnerSlice';
 import { Uri } from '@/types/types';
 import { ScreenSpaceKey } from '@/util/keys';
 
@@ -14,64 +17,94 @@ import { ScreenSpaceRenderableView } from './ScreenSpaceRenderableView';
 
 export function ScreenSpaceRenderablePanel() {
   const { t } = useTranslation('panel-screenspacerenderable');
+  const [selectedRenderable, setSelectedRenderable] = useState<Uri | null>(null);
 
   const screenSpacePropertyOwner = usePropertyOwner(ScreenSpaceKey);
-
-  const [selectedRenderable, setSelectedRenderable] = useState<Uri | null>(null);
+  const propertyOwners = useAppSelector((state) =>
+    propertyOwnerSelectors.selectEntities(state)
+  );
 
   const renderables = screenSpacePropertyOwner?.subowners ?? [];
 
+  function onItemClick(uri: Uri) {
+    setSelectedRenderable((prevSelected) => (prevSelected === uri ? null : uri));
+  }
+
+  function renderListItem(uri: Uri) {
+    return (
+      <Box
+        mb={5}
+        key={uri}
+        pl={'xs'}
+        style={{
+          borderLeft:
+            selectedRenderable == uri
+              ? 'var(--openspace-border-active)'
+              : 'var(--openspace-border-active-placeholder)',
+          backgroundColor:
+            selectedRenderable == uri ? 'var(--mantine-color-dark-7)' : undefined
+        }}
+      >
+        <ScreenSpaceRenderableListItem uri={uri} onClick={() => onItemClick(uri)} />
+      </Box>
+    );
+  }
+
   return (
     <>
-      <Group justify={'space-between'}>
-        <Title order={2}>{t('added-slides.title')}</Title>
-        <AddModal />
-      </Group>
-      <Divider my={'xs'} />
-
-      {renderables.length === 0 ? (
-        <Text>{t('added-slides.empty-slides')}</Text>
-      ) : (
-        <>
-          <ResizeableContent defaultHeight={150}>
-            <ScrollBox h={'100%'}>
-              {renderables.map((uri) => (
-                <Box
-                  mb={5}
-                  key={uri}
-                  pl={'xs'}
-                  style={{
-                    borderLeft:
-                      selectedRenderable == uri
-                        ? 'var(--openspace-border-active)'
-                        : 'var(--openspace-border-active-placeholder)',
-                    backgroundColor:
-                      selectedRenderable == uri
-                        ? 'var(--mantine-color-dark-7)'
-                        : undefined
-                  }}
-                >
-                  <ScreenSpaceRenderableListItem
-                    uri={uri}
-                    onClick={() =>
-                      selectedRenderable == uri
-                        ? setSelectedRenderable(null)
-                        : setSelectedRenderable(uri)
-                    }
-                  />
-                </Box>
-              ))}
-            </ScrollBox>
-          </ResizeableContent>
-          <Box pt={'xs'}>
-            {selectedRenderable ? (
-              <ScreenSpaceRenderableView uri={selectedRenderable} />
-            ) : (
-              <Text c={'dimmed'}>Select an item to view its details</Text>
-            )}
+      <FilterList>
+        <Group preventGrowOverflow={false} justify={'space-between'}>
+          <Box flex={1}>
+            <FilterList.InputField
+              placeHolderSearchText={t('added-slides.search-placeholder')}
+            />
           </Box>
-        </>
-      )}
+          <AddModal />
+        </Group>
+
+        {renderables.length === 0 ? (
+          // @TODO: Replace with Mantine's EmptyState component when library is updated
+          <Stack h={'100%'} w={'100%'} align={'center'} p={'lg'}>
+            <ThemeIcon size={70} variant={'transparent'} c={'dimmed'}>
+              <InsertPhotoIcon size={'100%'} />
+            </ThemeIcon>
+            <Text ta={'center'} c={'dimmed'}>
+              {t('added-slides.empty')}
+            </Text>
+            <Text ta={'center'} c={'dimmed'}>
+              {t('added-slides.tips')}
+            </Text>
+          </Stack>
+        ) : (
+          <>
+            <ResizeableContent defaultHeight={150}>
+              <FilterList.Favorites>
+                {renderables.map((uri) => renderListItem(uri))}
+              </FilterList.Favorites>
+
+              <FilterList.SearchResults
+                data={renderables}
+                renderElement={renderListItem}
+                matcherFunc={(uri: Uri, searchString: string) =>
+                  propertyOwners[uri]?.name
+                    .toLowerCase()
+                    .includes(searchString.toLowerCase())
+                }
+              >
+                <FilterList.SearchResults.VirtualList gap={'xs'} />
+              </FilterList.SearchResults>
+            </ResizeableContent>
+
+            <Box>
+              {selectedRenderable ? (
+                <ScreenSpaceRenderableView uri={selectedRenderable} />
+              ) : (
+                <Text c={'dimmed'}>{t('no-selection-hint')}</Text>
+              )}
+            </Box>
+          </>
+        )}
+      </FilterList>
     </>
   );
 }
